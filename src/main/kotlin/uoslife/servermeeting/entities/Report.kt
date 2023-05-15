cf364084c2f1d8219d8a5373fa8beee7a3b61385
pasly0920 <inwoo920621@gmail.com>
2023-05-15 16:55:22 +0900

    fix: report 수정 - Table 이름 수정 - 무의미한 Column name 반복 수정

--- src/main/kotlin/uoslife/servermeeting/entities/Report.kt
+++ src/main/kotlin/uoslife/servermeeting/entities/Report.kt
@@ -3,23 +3,22 @@ package uoslife.servermeeting.entities
 import jakarta.persistence.*
 
 @Entity
-@Table(name = "reports")
+@Table(name = "report")
 class Report(
     @Id
-    @Column(name = "report_id")
-    val reportId: Int,
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false, unique = true)
+    val id: Long? = null,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    val user: User,
+    val user: User? = null,
 
-    @Column(name = "report_category", nullable = false, length = 255)
+    @Column(nullable = false, length = 255)
     val reportCategory: String,
 
-    @Column(name = "report_text", nullable = false)
+    @Column(nullable = false)
     val reportText: String,
 
-    @Column(name = "admin_response", nullable = true)
-    val adminResponse: String? = null,
-
-) : BaseEntity() {}
+    val adminResponse: String,
+) : BaseEntity()
