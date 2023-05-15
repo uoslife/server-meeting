174ed9db37a736630ed0fc5827bfa38458632362
pasly0920 <inwoo920621@gmail.com>
2023-05-15 17:01:09 +0900

    fix: MeetingTeam 수정 - cascade 삭제 - 기타 Nullable Type 수정

--- src/main/kotlin/uoslife/servermeeting/entities/MeetingTeam.kt
+++ src/main/kotlin/uoslife/servermeeting/entities/MeetingTeam.kt
@@ -3,37 +3,37 @@ package uoslife.servermeeting.entities
 import jakarta.persistence.*
 
 @Entity
-@Table(name = "meeting_teams")
+@Table(name = "meeting_team")
 class MeetingTeam(
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(
-        name = "team_id",
-        nullable = false
-    ) val teamId: Long,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false, unique = true)
+    val id: Long? = null,
 
-    @Column(name = "season", nullable = false) val season: Int,
+    @Column(nullable = false)
+    val season: Int,
 
-    @Column(name = "code", nullable = false) val code: String,
+    @Column(nullable = false)
+    val code: String,
 
-    @OneToOne(cascade = [CascadeType.ALL], fetch = FetchType.LAZY, mappedBy = "team") val information: Information,
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "meetingTeam")
+    val information: Information? = null,
 
-    @OneToOne(
-        cascade = [CascadeType.ALL],
-        fetch = FetchType.LAZY,
-        mappedBy = "maleTeam"
-    ) val maleCompatibility: Compatibility,
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "maleTeam")
+    val maleCompatibility: Compatibility? = null,
 
-    @OneToOne(
-        cascade = [CascadeType.ALL],
-        fetch = FetchType.LAZY,
-        mappedBy = "femaleTeam"
-    ) val femaleCompatibility: Compatibility,
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "femaleTeam")
+    val femaleCompatibility: Compatibility? = null,
 
-    @OneToOne(cascade = [CascadeType.ALL], fetch = FetchType.LAZY, mappedBy = "maleTeam") val maleMatch: Match,
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "maleTeam")
+    val maleMatch: Match? = null,
 
-    @OneToOne(cascade = [CascadeType.ALL], fetch = FetchType.LAZY, mappedBy = "femaleTeam") val femaleMatch: Match,
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "femaleTeam")
+    val femaleMatch: Match? = null,
 
-    @OneToMany(cascade = [CascadeType.ALL], fetch = FetchType.LAZY, mappedBy = "team") val userTeams: List<UserTeam>,
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "team")
+    val userTeams: List<UserTeam> = mutableListOf(),
 
-    @OneToMany(cascade = [CascadeType.ALL], fetch = FetchType.LAZY, mappedBy = "team") val preference: List<Preference>,
-
-    )
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "meetingTeam")
+    val preference: List<Preference> = mutableListOf(),
+)
