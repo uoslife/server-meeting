8c8abbe34dde50e9590bd377c399fdc4431bff1d
pasly0920 <inwoo920621@gmail.com>
2023-05-15 16:53:51 +0900

    fix: User 수정 - Table 이름 수정 - id -> UUID 및 origin DB와 일치시키도록 변경 - cascade 및 orphanremoval 삭제

--- src/main/kotlin/uoslife/servermeeting/entities/User.kt
+++ src/main/kotlin/uoslife/servermeeting/entities/User.kt
@@ -1,49 +1,39 @@
 package uoslife.servermeeting.entities
 
 import jakarta.persistence.*
-import java.util.UUID
+import java.util.*
 
 @Entity
-@Table(name = "users")
+@Table(name = "`user`")
 class User(
 
+    // inject by origin db
     @Id
-    @Column(name = "id", nullable = false, updatable = false)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    val id: Long = 0,
+    @Column(nullable = false, unique = true)
+    val id: UUID? = null,
+
+    val birthYear: Int? = null,
+
+    val gender: GenderType? = null,
+
+    val phoneNumber: String,
+
+    val profilePicture: String,
+
+    val nickname: String,
+
+    val name: String,
 
     @OneToOne(fetch = FetchType.LAZY)
-    @PrimaryKeyJoinColumn
+    @JoinColumn(name = "department_id")
     val department: Department? = null,
 
-    val age: Int? = null,
+    @OneToMany(mappedBy = "user")
+    val noPreferDepartments: List<NoPreferDepartment> = mutableListOf(),
 
-    val gender: String? = null,
+    @OneToMany(mappedBy = "user")
+    val userTeams: List<UserTeam> = mutableListOf(),
 
-    val phoneNumber: String? = null,
-
-    val profilePicture: String? = null,
-
-    val nickname: String? = null,
-
-    val name: String? = null,
-
-    val authId: UUID? = null,
-
-    @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL], orphanRemoval = true, fetch = FetchType.LAZY)
-    val noPreferDepartments: MutableList<NoPreferDepartment> = mutableListOf(),
-
-    @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL], orphanRemoval = true, fetch = FetchType.LAZY)
-    val userTeam: MutableList<UserTeam> = mutableListOf(),
-
-    @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL], orphanRemoval = true, fetch = FetchType.LAZY)
-    val report: MutableList<Report> = mutableListOf(),
-
-    @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL], orphanRemoval = true, fetch = FetchType.LAZY)
-    val payment: MutableList<Payment> = mutableListOf(),
-
-
-
-
-
-) : BaseEntity() {}
+    @OneToMany(mappedBy = "user")
+    val reports: List<Report> = mutableListOf(),
+) : BaseEntity()
