a0a3be0a85f119b8743fa2d9efe962820a31db96
pasly0920 <inwoo920621@gmail.com>
2023-05-16 15:43:37 +0900

    feat: Argument 관련 FieldErrorDTO 및 Error 전송을 위한 ErrorResponse 추가

---
+++ src/main/kotlin/uoslife/servermeeting/global/error/ErrorResponse.kt
@@ -0,0 +1,15 @@
+package uoslife.servermeeting.global.error
+
+import uoslife.servermeeting.global.error.exception.ErrorCode
+
+data class ErrorResponse(
+    val message: String,
+    val status: Int,
+    val code: String,
+) {
+    constructor(errorCode: ErrorCode) : this(
+        message = errorCode.message,
+        status = errorCode.status,
+        code = errorCode.code,
+    )
+}
