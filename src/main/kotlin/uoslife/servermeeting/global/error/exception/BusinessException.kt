1d0e7384ce74e145488cf72e345ef9b1a2483946
pasly0920 <inwoo920621@gmail.com>
2023-05-16 15:44:04 +0900

    feat: Base Exception 추가

---
+++ src/main/kotlin/uoslife/servermeeting/global/error/exception/BusinessException.kt
@@ -0,0 +1,14 @@
+package uoslife.servermeeting.global.error.exception
+
+open class BusinessException : RuntimeException {
+
+    val errorCode: ErrorCode
+
+    constructor(message: String, errorCode: ErrorCode) : super(message) {
+        this.errorCode = errorCode
+    }
+
+    constructor(errorCode: ErrorCode) : super(errorCode.message) {
+        this.errorCode = errorCode
+    }
+}
