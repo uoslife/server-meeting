a1ee276d1e0e272f1a647c84635522f1e9157812
pasly0920 <inwoo920621@gmail.com>
2023-05-17 09:32:53 +0900

    feat: Cookie 값 추출 함수 추가

---
+++ src/main/kotlin/uoslife/servermeeting/global/util/CookieParser.kt
@@ -0,0 +1,20 @@
+package uoslife.servermeeting.global.util
+
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.stereotype.Component
+
+@Component
+class CookieParser {
+
+    fun extractCookieValue(request: HttpServletRequest, cookieName: String): String? {
+        val cookies = request.cookies
+        if (cookies != null) {
+            for (cookie in cookies) {
+                if (cookie.name == cookieName) {
+                    return cookie.value
+                }
+            }
+        }
+        return null
+    }
+}
