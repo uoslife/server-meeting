327965407e1174a605e65d7f1ff66fb138bcdb35
Marsboy02 <marsboy0619@gmail.com>
2023-05-24 23:15:07 +0900

    feat: user api 스키마 설계

---
+++ src/main/kotlin/uoslife/servermeeting/domain/user/application/request/UserUpdateRequestDto.kt
@@ -0,0 +1,3 @@
+package uoslife.servermeeting.domain.user.application.request
+
+data class UserUpdateRequestDto(val request: String)
---
+++ src/main/kotlin/uoslife/servermeeting/domain/user/application/response/UserUpdateResponseDto.kt
@@ -0,0 +1,3 @@
+package uoslife.servermeeting.domain.user.application.response
+
+data class UserUpdateResponseDto (val message: String)
