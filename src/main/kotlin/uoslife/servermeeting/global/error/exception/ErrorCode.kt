9187c3d3253cc5f9ee02c3809aca833114da2904
pasly0920 <inwoo920621@gmail.com>
2023-05-24 21:56:15 +0900

    feat: Single Meeting 관련 ErrorCode 추가

--- src/main/kotlin/uoslife/servermeeting/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/uoslife/servermeeting/global/error/exception/ErrorCode.kt
@@ -24,6 +24,30 @@ enum class ErrorCode(val code: String, val message: String, var status: Int) {
     // Department
     DEPARTMENT_NOT_FOUND("D01", "Department is not Found.", HttpStatus.BAD_REQUEST.value()),
 
+    // Meeting
+    USER_TEAM_NOT_FOUND("M01", "User Team is not Found.", HttpStatus.BAD_REQUEST.value()),
+    USER_ALREADY_HAVE_TEAM("M02", "User already have Team.", HttpStatus.BAD_REQUEST.value()),
+    ONLY_TEAM_LEADER_CAN_CREATE_TEAM("M03", "Only Team Leader can Create Team.", HttpStatus.BAD_REQUEST.value()),
+    IN_SINGLE_MEETING_TEAM_NO_JOIN_TEAM(
+        "M04",
+        "In Single Meeting Team, you can't Join Team.",
+        HttpStatus.BAD_REQUEST.value(),
+    ),
+    IN_SINGLE_MEETING_TEAM_ONLY_ONE_USER(
+        "M05",
+        "In Single Meeting Team, only One User Exist.",
+        HttpStatus.BAD_REQUEST.value(),
+    ),
+    MEETING_TEAM_NOT_FOUND("M06", "Meeting Team is not Found.", HttpStatus.BAD_REQUEST.value()),
+    INFORMATION_NOT_FOUND("M07", "Information is not Found.", HttpStatus.BAD_REQUEST.value()),
+    PREFERENCE_NOT_FOUND("M08", "Preference is not Found.", HttpStatus.BAD_REQUEST.value()),
+    ONLY_TEAM_LEADER_CAN_UPDATE_TEAM_INFORMATION(
+        "M09",
+        "Only Team Leader can Update Team Information.",
+        HttpStatus.BAD_REQUEST.value(),
+    ),
+    ONLY_TEAM_LEADER_CAN_DELETE_TEAM("M10", "Only Team Leader can Delete Team.", HttpStatus.BAD_REQUEST.value()),
+
     // External API
     EXTERNAL_API_FAILED("E01", "External API Request is failed.", HttpStatus.INTERNAL_SERVER_ERROR.value()),
 }
