80ffdb4e32f919d99136f3a464ee1bf0dfbfa6f4
pasly0920 <inwoo920621@gmail.com>
2023-05-24 21:57:12 +0900

    feat: 1대1, 3대3 미팅 Service 계층 공통 Interface 추가

---
+++ src/main/kotlin/uoslife/servermeeting/domain/meeting/domain/service/BaseMeetingService.kt
@@ -0,0 +1,22 @@
+package uoslife.servermeeting.domain.meeting.domain.service
+
+import uoslife.servermeeting.domain.meeting.application.response.MeetingTeamInformationGetResponse
+import uoslife.servermeeting.domain.meeting.application.response.MeetingTeamUserListGetResponse
+import java.util.*
+
+interface BaseMeetingService {
+    fun createMeetingTeam(userUUID: UUID, name: String? = null): String?
+    fun joinMeetingTeam(userUUID: UUID, code: String)
+    fun getMeetingTeamUserList(userUUID: UUID): MeetingTeamUserListGetResponse
+    fun updateMeetingTeamInformation(
+        userUUID: UUID,
+        informationDistance: String,
+        informationFilter: String,
+        informationMeetingTime: String,
+        preferenceDistance: String,
+        preferenceFilter: String,
+    )
+
+    fun getMeetingTeamInformation(userUUID: UUID): MeetingTeamInformationGetResponse
+    fun deleteMeetingTeam(userUUID: UUID)
+}
