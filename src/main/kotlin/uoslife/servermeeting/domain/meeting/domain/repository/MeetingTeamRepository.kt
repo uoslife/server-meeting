c3b71ef5642f7e567e7d6532b5dd684c0afae938
pasly0920 <inwoo920621@gmail.com>
2023-05-26 03:03:17 +0900

    feat: Triple Meeting 특성상 Code를 통한 조회 및 Code 중복 조회 추가

--- src/main/kotlin/uoslife/servermeeting/domain/meeting/domain/repository/MeetingTeamRepository.kt
+++ src/main/kotlin/uoslife/servermeeting/domain/meeting/domain/repository/MeetingTeamRepository.kt
@@ -3,4 +3,7 @@ package uoslife.servermeeting.domain.meeting.domain.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import uoslife.servermeeting.domain.meeting.domain.entity.MeetingTeam
 
-interface MeetingTeamRepository : JpaRepository<MeetingTeam, Long>
+interface MeetingTeamRepository : JpaRepository<MeetingTeam, Long> {
+    fun existsByCode(code: String): Boolean
+    fun findByCode(code: String): MeetingTeam?
+}
