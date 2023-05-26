3d4d81c96e3c999f0c0f78c43e18bb563b956b8c
Marsboy02 <marsboy0619@gmail.com>
2023-05-26 13:54:55 +0900

    style: 중괄호 제거

--- src/main/kotlin/uoslife/servermeeting/domain/user/domain/exception/ExistingUserNotFoundException.kt
+++ src/main/kotlin/uoslife/servermeeting/domain/user/domain/exception/ExistingUserNotFoundException.kt
@@ -3,5 +3,4 @@ package uoslife.servermeeting.domain.user.domain.exception
 import uoslife.servermeeting.global.error.exception.EntityNotFoundException
 import uoslife.servermeeting.global.error.exception.ErrorCode
 
-class ExistingUserNotFoundException : EntityNotFoundException(ErrorCode.USER_NOT_FOUND){
-}
+class ExistingUserNotFoundException : EntityNotFoundException(ErrorCode.USER_NOT_FOUND)
