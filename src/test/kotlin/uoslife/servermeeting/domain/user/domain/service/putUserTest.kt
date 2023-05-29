d852811114bdc50d293341637610048aeb79d04b
Marsboy02 <marsboy0619@gmail.com>
2023-05-29 20:00:28 +0900

    test: put user 테스트 코드 추가

--- src/test/kotlin/uoslife/servermeeting/domain/user/domain/service/putUserTest.kt
+++ src/test/kotlin/uoslife/servermeeting/domain/user/domain/service/putUserTest.kt
@@ -28,4 +28,19 @@ class putUserTest : UserServiceTest() {
         assertThat(updatedUser.nickname).isNotNull()
         assertThat(updatedUser.height).isEqualTo(0)
     }
+
+    @Test
+    fun `user 데이터의 uuid가 일치하는 지 확인한다`() {
+        // given
+        val userUUID = userRepository.findAll().first().id!!
+
+        // when
+        userService.resetUser(userUUID)
+        entityManager.flush()
+        entityManager.clear()
+        val updatedUser = userRepository.findByIdOrNull(userUUID)!!
+
+        // then
+        assertThat(updatedUser.id).isEqualTo(userUUID)
+    }
 }
