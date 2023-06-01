be6ae5dac40120507539e11e6ee97cefcf748db4
SangMinLeeAI <sangmin.lee.ai@gmail.com>
2023-06-01 16:08:59 +0900

    feat: match 결과 생성
    user id로 matched team 조회

---
+++ src/main/kotlin/uoslife/servermeeting/domain/dao/MatchedDao.kt
@@ -0,0 +1,30 @@
+package uoslife.servermeeting.domain.dao
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import jakarta.transaction.Transactional
+import org.springframework.stereotype.Repository
+import uoslife.servermeeting.domain.match.domain.entity.QMatch.match
+import uoslife.servermeeting.domain.meeting.domain.entity.MeetingTeam
+import uoslife.servermeeting.domain.user.domain.entity.enums.GenderType
+import uoslife.servermeeting.domain.user.domain.entity.enums.GenderType.FEMALE
+import uoslife.servermeeting.domain.user.domain.entity.enums.GenderType.MALE
+
+@Repository
+@Transactional
+class MatchedDao(private val queryFactory: JPAQueryFactory) {
+
+    fun findMatchedTeamByTeamAndGender(
+        meetingTeam: MeetingTeam,
+        gender: GenderType,
+    ): MeetingTeam? {
+        return when (gender) {
+            MALE -> queryFactory.selectFrom(match)
+                .where(match.maleTeam.eq(meetingTeam))
+                .fetchOne()?.femaleTeam
+
+            FEMALE -> queryFactory.selectFrom(match)
+                .where(match.femaleTeam.eq(meetingTeam))
+                .fetchOne()?.maleTeam
+        }
+    }
+}
--- src/main/kotlin/uoslife/servermeeting/domain/match/domain/service/impl/MatchingService.kt
+++ src/main/kotlin/uoslife/servermeeting/domain/match/domain/service/impl/MatchingService.kt
@@ -1,70 +1,51 @@
 package uoslife.servermeeting.domain.match.domain.service.impl
 
-import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import uoslife.servermeeting.domain.dao.MatchedDao
+import uoslife.servermeeting.domain.meeting.application.response.MeetingTeamInformationGetResponse
+import uoslife.servermeeting.domain.meeting.domain.dao.UserTeamDao
+import uoslife.servermeeting.domain.meeting.domain.entity.enums.TeamType.*
+import uoslife.servermeeting.domain.meeting.domain.exception.InformationNotFoundException
+import uoslife.servermeeting.domain.meeting.domain.exception.MeetingTeamNotFoundException
+import uoslife.servermeeting.domain.meeting.domain.exception.UserTeamNotFoundException
+import uoslife.servermeeting.domain.meeting.domain.repository.MeetingTeamRepository
+import uoslife.servermeeting.domain.meeting.domain.service.impl.SingleMeetingService
+import uoslife.servermeeting.domain.meeting.domain.service.impl.TripleMeetingService
+import uoslife.servermeeting.domain.user.domain.exception.UserNotFoundException
+import uoslife.servermeeting.domain.user.domain.repository.UserRepository
+import java.util.*
 
 @Service
-@Qualifier("matchingService")
-class MatchingService() {
-    fun assignProposersToReceivers(
-        receivers: List<Receiver>,
-        proposers: List<Proposer>,
-    ): List<Pair<Receiver, Proposer>> {
-        val matches = mutableListOf<Pair<Receiver, Proposer>>()
-
-        do {
-            var receiver = receivers.firstOrNull {
-                it.capacity > it.match.size && it.preferences.any { proposer -> proposer.match == null }
+class MatchingService(
+    private val userRepository: UserRepository,
+    private val userTeamDao: UserTeamDao,
+    private val meetingTeamRepository: MeetingTeamRepository,
+    private val matchedDao: MatchedDao,
+    private val singleMeetingService: SingleMeetingService,
+    private val tripleMeetingService: TripleMeetingService,
+) {
+    fun getMatchedMeetingTeam(userUUID: UUID): MeetingTeamInformationGetResponse {
+        val user = userRepository.findByIdOrNull(userUUID) ?: throw UserNotFoundException()
+        val userTeam = userTeamDao.findByUser(user) ?: throw UserTeamNotFoundException()
+        val meetingTeam =
+            meetingTeamRepository.findByIdOrNull(userTeam.team.id!!) ?: throw MeetingTeamNotFoundException()
+        val opponentTeam =
+            matchedDao.findMatchedTeamByTeamAndGender(meetingTeam, user.gender) ?: throw InformationNotFoundException()
+        return when (userTeam.type) {
+            SINGLE -> {
+                val opponentUserTeam =
+                    userTeamDao.findByTeam(opponentTeam)
+                val opponentUser = opponentUserTeam.first().user ?: throw UserNotFoundException()
+                singleMeetingService.getMeetingTeamInformation(opponentUser.id ?: throw UserNotFoundException())
             }
-            var proposer = receiver?.preferences?.firstOrNull()
 
-            if (receiver == null || proposer == null) {
-                break
-            }
-
-            deletePairFromMatch(receiver, proposer)
-
-            makeNewPair(proposer, receiver)
-
-            // For each successor, `s` to `receiver` in the preference list of `proposer`,
-            // delete the pair `(p, s)` from the game.
-            proposer.preferences = deletePreferenceOfReceiverBySuccessors(proposer, receiver)
-        } while (true)
-
-        receivers.forEach { receiver ->
-            receiver.match.forEach { proposer ->
-                matches.add(Pair(receiver, proposer))
+            TRIPLE -> {
+                val opponentUserTeam =
+                    userTeamDao.findByTeam(opponentTeam)
+                val opponentUser = opponentUserTeam.first().user ?: throw UserNotFoundException()
+                tripleMeetingService.getMeetingTeamInformation(opponentUser.id ?: throw UserNotFoundException())
             }
         }
-        return matches
-    }
-
-    private fun makeNewPair(
-        proposer: Proposer,
-        receiver: Receiver,
-    ) {
-        proposer.match = receiver
-        receiver.match.add(proposer)
-    }
-
-    fun deletePairFromMatch(receiver: Receiver, proposer: Proposer) {
-        proposer.match?.match?.remove(proposer)
-        proposer.preferences.remove(receiver)
-    }
-
-    fun deletePreferenceOfReceiverBySuccessors(proposer: Proposer, successor: Receiver): MutableList<Receiver> {
-        return proposer.preferences.subList(
-            proposer.preferences.indexOf(successor) + 1,
-            proposer.preferences.size,
-        )
-    }
-
-    fun allocateRoleOnMatching() {
-        // count MeetingTeam on gender and allocate role.
-        // gender that has more count will be allocated to proposer
-    }
-
-    fun makeMeetingTeamToPlayer() {
-        // make MeetingTeam to Player
     }
 }
