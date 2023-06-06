90b2a19ccc1b717ef15443e15712aab1ddc56167
Marsboy02 <marsboy0619@gmail.com>
2023-06-06 15:35:00 +0900

    test: user validator 추가에 따라 테스트 코드 작성

--- src/test/kotlin/uoslife/servermeeting/domain/user/domain/service/putUserTest.kt
+++ src/test/kotlin/uoslife/servermeeting/domain/user/domain/service/putUserTest.kt
@@ -3,8 +3,13 @@ package uoslife.servermeeting.domain.user.domain.service
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.springframework.data.repository.findByIdOrNull
+import uoslife.servermeeting.domain.user.application.request.UserUpdateRequest
 import uoslife.servermeeting.domain.user.domain.common.UserServiceTest
+import uoslife.servermeeting.domain.user.domain.entity.enums.DepartmentNameType
+import uoslife.servermeeting.domain.user.domain.entity.enums.GenderType
+import uoslife.servermeeting.domain.user.domain.entity.enums.StudentType
 import uoslife.servermeeting.domain.user.domain.exception.ExistingUserNotFoundException
+import uoslife.servermeeting.domain.user.domain.exception.UserAlreadyResetException
 import java.util.UUID
 
 class putUserTest : UserServiceTest() {
@@ -12,13 +17,20 @@ class putUserTest : UserServiceTest() {
     @Test
     fun `정상적으로 User 데이터가 put 됐는지 확인한다`() {
         // given
-        val userUUID = userRepository.findAll().first().id!!
-
-        // when
-        userService.resetUser(userUUID)
+        val user = userRepository.findAll().first()
+        val updateData = UserUpdateRequest(
+            2000, GenderType.MALE, DepartmentNameType.BUSINESS, "kakao", StudentType.GRADUATE,
+            false, "chicken", "ENTJ", "coding", 173, "nickname0",
+        )
+        userUpdateDao.updateUser(updateData, user)
         entityManager.flush()
         entityManager.clear()
-        val updatedUser = userRepository.findByIdOrNull(userUUID)!!
+
+        // when
+        userService.resetUser(user.id!!)
+        entityManager.flush()
+        entityManager.clear()
+        val updatedUser = userRepository.findByIdOrNull(user.id)!!
 
         // then
         assertThat(updatedUser.department).isNull()
@@ -33,18 +45,13 @@ class putUserTest : UserServiceTest() {
     }
 
     @Test
-    fun `user 데이터의 uuid가 일치하는 지 확인한다`() {
+    fun `이미 reset된 user에 대한 reset 요청에 에러를 반환한다`() {
         // given
         val userUUID = userRepository.findAll().first().id!!
 
-        // when
-        userService.resetUser(userUUID)
-        entityManager.flush()
-        entityManager.clear()
-        val updatedUser = userRepository.findByIdOrNull(userUUID)!!
-
-        // then
-        assertThat(updatedUser.id).isEqualTo(userUUID)
+        // when & then
+        assertThatThrownBy { userService.resetUser(userUUID) }
+            .isInstanceOf(UserAlreadyResetException::class.java)
     }
 
     @Test
