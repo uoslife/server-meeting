6146a99956aaeceb7ca956bf554d2404594d837b
SangMinLeeAI <sangmin.lee.ai@gmail.com>
2023-06-26 20:27:55 +0900

    refactor: algorithm 부분 삭제
    사용하지 않은 부분 삭제

--- src/main/kotlin/uoslife/servermeeting/domain/match/domain/service/impl/matchingrole/Receiver.kt
+++
@@ -1,8 +0,0 @@
-package uoslife.servermeeting.domain.match.domain.service.impl.matchingrole
-
-class Receiver(val id: Long, var capacity: Int = 0) {
-
-    var preferences: MutableList<Proposer> = mutableListOf()
-
-    var match: MutableList<Proposer> = mutableListOf()
-}
