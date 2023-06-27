3e06c1622dda41c440efc6c82f24afc7d4b23430
SangMinLeeAI <96653505+SangMinLeeAI@users.noreply.github.com>
2023-06-27 13:48:59 +0900

    refactor: matching package refactor (#24)
    * feat: sharpley-gale 알고리즘
    
    * refactor: 중복 클래스 제거
    
    * refactor: domain으로 이동
    
    패키지 구조에 어긋나는 패키지 구조 수정
    
    * refactor: domain으로 이동
    
    패키지 구조에 어긋나는 패키지 구조 수정
    
    * refactor: algorithm 부분 삭제
    
    사용하지 않은 부분 삭제

--- src/main/kotlin/uoslife/servermeeting/domain/match/domain/service/impl/matchingrole/Proposer.kt
+++
@@ -1,8 +0,0 @@
-package uoslife.servermeeting.domain.match.domain.service.impl.matchingrole
-
-class Proposer(val id: Long) {
-
-    var preferences: MutableList<Receiver> = mutableListOf()
-
-    var match: Receiver? = null
-}
