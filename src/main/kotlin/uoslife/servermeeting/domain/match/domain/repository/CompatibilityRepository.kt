b54ad327ea3d2f3a298e10f70685f22e4300dae1
SangMinLeeAI <sangmin.lee.ai@gmail.com>
2023-11-10 20:09:28 +0900

    feat: change compatibility to jsonb

--- src/main/kotlin/uoslife/servermeeting/domain/match/domain/repository/CompatibilityRepository.kt
+++
@@ -1,6 +0,0 @@
-package uoslife.servermeeting.domain.match.domain.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import uoslife.servermeeting.domain.match.domain.entity.Compatibility
-
-interface CompatibilityRepository : JpaRepository<Compatibility, Long>
