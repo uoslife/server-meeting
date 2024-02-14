da662adc23df76f2a266e3098b64ea984a9d21f1
sangmin <sangmin.lee.ai@gmail.com>
2024-02-15 02:15:40 +0900

    fix: set comments

--- src/main/kotlin/uoslife/servermeeting/global/config/SecurityConfig.kt
+++ src/main/kotlin/uoslife/servermeeting/global/config/SecurityConfig.kt
@@ -2,22 +2,15 @@ package uoslife.servermeeting.global.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.CorsUtils
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import uoslife.servermeeting.global.auth.RestAuthenticationEntryPoint
-import uoslife.servermeeting.global.auth.cookie.CookieAuthFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig(private val cookieAuthFilter: CookieAuthFilter) {
+class SecurityConfig() {
 
     @Bean
     fun configurationSource(): CorsConfigurationSource? {
@@ -39,43 +32,6 @@ class SecurityConfig(private val cookieAuthFilter: CookieAuthFilter) {
         source.registerCorsConfiguration("/**", configuration)
 
         return source
-    }
-
-    @Bean
-    @Throws(Exception::class)
-    fun filterChain(http: HttpSecurity): SecurityFilterChain? {
-        http.cors().configurationSource(configurationSource())
-
-        http
-            .httpBasic()
-            .disable()
-            .csrf()
-            .disable()
-            .formLogin()
-            .disable()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .headers()
-            .frameOptions()
-            .disable()
-            .and()
-            .exceptionHandling()
-            .authenticationEntryPoint(
-                RestAuthenticationEntryPoint(),
-            ) // 인증, 인가가 되지 않은 요청 발생시
-            .and()
-            .authorizeHttpRequests()
-            .requestMatchers(CorsUtils::isPreFlightRequest)
-            .permitAll() // CORS preflight 요청 허용
-            .requestMatchers("/api/swagger-ui/**", "/api/api-docs/**")
-            .permitAll() // Swagger 허용 url
-            .requestMatchers("/api/**")
-            .hasRole("USER") // 모든 api 요청에 대해 권한 필요
-
-        http.addFilterBefore(cookieAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
-
-        return http.build()
     }
 
     @Bean
--- src/main/kotlin/uoslife/servermeeting/meetingteam/dao/UserTeamDao.kt
+++ src/main/kotlin/uoslife/servermeeting/meetingteam/dao/UserTeamDao.kt
@@ -4,11 +4,8 @@ import com.querydsl.jpa.impl.JPAQueryFactory
 import jakarta.persistence.EntityManager
 import jakarta.transaction.Transactional
 import org.springframework.stereotype.Repository
-import uoslife.servermeeting.meetingteam.entity.MeetingTeam
 import uoslife.servermeeting.domain.meeting.domain.entity.QUserTeam.userTeam
-import uoslife.servermeeting.domain.meeting.domain.entity.UserTeam
-import uoslife.servermeeting.meetingteam.entity.enums.TeamType
-import uoslife.servermeeting.user.entity.User
+import uoslife.servermeeting.meetingteam.entity.MeetingTeam
 
 @Repository
 @Transactional
@@ -16,48 +13,48 @@ class UserTeamDao(
     private val queryFactory: JPAQueryFactory,
     private val entityManager: EntityManager,
 ) {
-    fun saveUserTeam(userTeam: UserTeam) {
-        entityManager.persist(userTeam)
-    }
-
-    fun findByUser(user: User): UserTeam? {
-        return queryFactory.selectFrom(userTeam).where(userTeam.user.eq(user)).fetchOne()
-    }
-
-    fun findByUserWithMeetingTeam(user: User, teamType: TeamType): UserTeam? {
-        return queryFactory
-            .selectFrom(userTeam)
-            .join(userTeam.team)
-            .fetchJoin()
-            .where(userTeam.user.eq(user))
-            .where(userTeam.type.eq(teamType))
-            .fetchOne()
-    }
-
-    fun findByUserAndTeam(user: User, meetingTeam: MeetingTeam): UserTeam? {
-        return queryFactory
-            .selectFrom(userTeam)
-            .where(userTeam.user.eq(user), userTeam.team.eq(meetingTeam))
-            .fetchOne()
-    }
-
-    fun findByTeam(meetingTeam: MeetingTeam): List<UserTeam> {
-        return queryFactory
-            .selectFrom(userTeam)
-            .join(userTeam.user)
-            .fetchJoin()
-            .where(userTeam.team.eq(meetingTeam))
-            .fetch()
-    }
-
-    fun findByTeamAndisLeader(meetingTeam: MeetingTeam, isLeader: Boolean): UserTeam? {
-        return queryFactory
-            .selectFrom(userTeam)
-            .join(userTeam.user)
-            .fetchJoin()
-            .where(userTeam.team.eq(meetingTeam), userTeam.isLeader.eq(isLeader))
-            .fetchOne()
-    }
+//    fun saveUserTeam(userTeam: UserTeam) {
+//        entityManager.persist(userTeam)
+//    }
+//
+//    fun findByUser(user: User): UserTeam? {
+//        return queryFactory.selectFrom(userTeam).where(userTeam.user.eq(user)).fetchOne()
+//    }
+//
+//    fun findByUserWithMeetingTeam(user: User, teamType: TeamType): UserTeam? {
+//        return queryFactory
+//            .selectFrom(userTeam)
+//            .join(userTeam.team)
+//            .fetchJoin()
+//            .where(userTeam.user.eq(user))
+//            .where(userTeam.type.eq(teamType))
+//            .fetchOne()
+//    }
+//
+//    fun findByUserAndTeam(user: User, meetingTeam: MeetingTeam): UserTeam? {
+//        return queryFactory
+//            .selectFrom(userTeam)
+//            .where(userTeam.user.eq(user), userTeam.team.eq(meetingTeam))
+//            .fetchOne()
+//    }
+//
+//    fun findByTeam(meetingTeam: MeetingTeam): List<UserTeam> {
+//        return queryFactory
+//            .selectFrom(userTeam)
+//            .join(userTeam.user)
+//            .fetchJoin()
+//            .where(userTeam.team.eq(meetingTeam))
+//            .fetch()
+//    }
+//
+//    fun findByTeamAndisLeader(meetingTeam: MeetingTeam, isLeader: Boolean): UserTeam? {
+//        return queryFactory
+//            .selectFrom(userTeam)
+//            .join(userTeam.user)
+//            .fetchJoin()
+//            .where(userTeam.team.eq(meetingTeam), userTeam.isLeader.eq(isLeader))
+//            .fetchOne()
+//    }
 
     fun countByTeam(meetingTeam: MeetingTeam): Int {
         return queryFactory.selectFrom(userTeam).where(userTeam.team.eq(meetingTeam)).fetch().size
