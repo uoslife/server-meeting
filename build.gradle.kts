ff34f644cf7e6f08aeaf60c1f7dd4b96b90ae0fb
sangmin <sangmin.lee.ai@gmail.com>
2024-02-15 20:41:36 +0900

    build: fix version of kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,11 +67,11 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
     // kotest
-    testImplementation("io.kotest:kotest-runner-junit5:5.5.5")
-    testImplementation("io.kotest:kotest-assertions-core:5.5.5")
-    testImplementation("io.kotest:kotest-extensions-spring:4.4.3")
+    testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
+    testImplementation("io.kotest:kotest-assertions-core:5.8.0")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
     testImplementation("io.mockk:mockk:1.13.5")
-    testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:5.5.5")
+    testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:2.0.2")
 
     // hibernate annotation
     implementation("com.vladmihalcea:hibernate-types-60:2.21.1")
