6f631a1dc4d1ec50407133dedefeb6b3865da8c9
naingyu <sktmdgus1212@naver.com>
2024-02-20 18:41:01 +0900

    docs: add annotation in paymentStatus

--- src/main/kotlin/uoslife/servermeeting/meetingteam/entity/enums/PaymentStatus.kt
+++ src/main/kotlin/uoslife/servermeeting/meetingteam/entity/enums/PaymentStatus.kt
@@ -2,11 +2,11 @@ package uoslife.servermeeting.meetingteam.entity.enums
 
 enum class PaymentStatus {
     NONE,
-    REQUEST,
-    COMPLETE_PAYMENT,
-    CANCEL_REQUEST,
-    CANCEL_ACCEPTANCE,
-    WAITING_PAYMENT,
-    CANCEL_PARTIAL_REFUND,
-    FAILED_REFUND
+    REQUEST, // 요청
+    COMPLETE_PAYMENT, // 결제완료
+    CANCEL_REQUEST, // 요청취소
+    CANCEL_ACCEPTANCE, // 승인취소
+    WAITING_PAYMENT, // 결제대기
+    CANCEL_PARTIAL_REFUND, // 부분취소
+    FAILED_REFUND // 환불실패
 }
