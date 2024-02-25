aec512224da8a084f735879e9ed3eab7bdde8edd
UDADDY <gustmd5715@gmail.com>
2024-02-25 16:26:17 +0900

    feat: validate email only 경희대 and 한국외대

--- src/main/kotlin/uoslife/servermeeting/cert/dto/request/CertifyRequest.kt
+++
@@ -1,9 +0,0 @@
-package uoslife.servermeeting.cert.dto.request
-
-import jakarta.validation.constraints.NotBlank
-import uoslife.servermeeting.cert.dto.University
-
-data class CertifyRequest(
-    @NotBlank val university: University,
-    @NotBlank val email: String,
-)
