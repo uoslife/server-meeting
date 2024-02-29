e3282a511bc3fdf2d04e3ce9ac45a84428449079
UDADDY <gustmd5715@gmail.com>
2024-02-29 13:43:38 +0900

    feat: change unique code only number

--- src/main/kotlin/uoslife/servermeeting/meetingteam/util/UniqueCodeGenerator.kt
+++ src/main/kotlin/uoslife/servermeeting/meetingteam/util/UniqueCodeGenerator.kt
@@ -2,14 +2,14 @@ package uoslife.servermeeting.meetingteam.util
 
 import java.security.SecureRandom
 import org.springframework.stereotype.Component
-import uoslife.servermeeting.certification.repository.VerificationRepository
+import uoslife.servermeeting.certification.repository.VerificationRedisRepository
 import uoslife.servermeeting.meetingteam.exception.TeamCodeGenerateFailedException
 import uoslife.servermeeting.meetingteam.repository.MeetingTeamRepository
 
 @Component
 class UniqueCodeGenerator(
     private val meetingTeamRepository: MeetingTeamRepository,
-    private val verificationRepository: VerificationRepository,
+    private val verificationRedisRepository: VerificationRedisRepository,
 ) {
     fun getUniqueTeamCode(): String {
         val characters = ('A'..'Z') + ('0'..'9') // A-Z, 0-9 문자열 리스트
@@ -41,20 +41,12 @@ class UniqueCodeGenerator(
     }
 
     fun getUniqueCertCode(): String {
-        val characters = ('A'..'Z') + ('0'..'9') // A-Z, 0-9 문자열 리스트
-        val random = SecureRandom.getInstanceStrong()
+        val characters: List<Char> = ('0'..'9').toList() // A-Z, 0-9 문자열 리스트
+        val random: SecureRandom = SecureRandom.getInstanceStrong()
 
-        var code: String
-        var isDuplicate: Boolean
-        do { // DB 내 코드 중복이 나오지 않을 때까지 6자리 숫자 난수 코드 생성
-            code =
-                (1..6)
-                    .map { characters[random.nextInt(characters.size)] } // 리스트에서 임의로 선택
-                    .joinToString("") // 선택된 문자들을 연결하여 문자열 생성
-
-            // DB 내 코드의 중복 체크
-            isDuplicate = verificationRepository.existsByCode(code)
-        } while (isDuplicate)
+        var code: String = (1..6)
+            .map { characters[random.nextInt(characters.size)] }
+            .joinToString { "" }
 
         return code
     }
