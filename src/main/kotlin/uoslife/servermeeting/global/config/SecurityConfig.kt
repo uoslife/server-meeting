010c863a9a7308cda394a9834babc39a69d14102
UDADDY <gustmd5715@gmail.com>
2024-03-05 21:16:32 +0900

    feat: copy and paste from rebuild-server

--- src/main/kotlin/uoslife/servermeeting/global/config/SecurityConfig.kt
+++ src/main/kotlin/uoslife/servermeeting/global/config/SecurityConfig.kt
@@ -4,17 +4,50 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.CorsUtils
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig() {
+    @Bean
+    @Throws(Exception::class)
+    fun filterChain(http: HttpSecurity): SecurityFilterChain? {
+        http.cors().configurationSource(configurationSource())
+
+        http.httpBasic().disable()
+            .csrf().disable()
+            .cors().configurationSource(configurationSource())
+            .and()
+            .formLogin().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .headers().frameOptions().disable()
+            .and()
+            .exceptionHandling()
+//            .authenticationEntryPoint(
+//                RestAuthenticationEntryPoint(),
+//            ) // 인증, 인가가 되지 않은 요청 발생시
+            .and()
+            .authorizeHttpRequests()
+            .requestMatchers(CorsUtils::isPreFlightRequest).permitAll() // CORS preflight 요청 허용
+            .requestMatchers("/api/swagger-ui/**", "/api/api-docs/**").permitAll() // Swagger 허용 url
+            .requestMatchers("/api/**").hasRole("USER") // 모든 api 요청에 대해 권한 필요
+
+//        http
+//            .addFilterBefore(JwtAuthenticationFilter(jwtUtils), UsernamePasswordAuthenticationFilter::class.java)
+
+        return http.build()
+    }
     @Bean
     @ConditionalOnProperty(name = ["spring.h2.console.enabled"], havingValue = "true")
     fun configureH2ConsoleEnable(): WebSecurityCustomizer {
