f88b0986b7201232e1b8dde4d831007a31238c38
InGyu Na <43513001+sktmdgus1212@users.noreply.github.com>
2024-03-05 19:20:17 +0900

    feat: create payment api (#37)
    * feat: hibernate-types for postgres jsonb type
    
    * feat: change entity to jsonb columns
    
    * feat: change service and test followed by entity changes
    
    * feat: change meeting team column
    
    - make information jsonb with dynamic json mapping
    - delete preference and integrate with information columns
    
    * test: change test code due to change in information type
    
    * feat: add email and verified field for school verification
    
    * feat: add payment to userTeam
    
    * feat: add pk to userTeam
    
    * feat: set foreign key in payments to meeting team not in user team
    
    * feat: change compatibility to jsonb
    
    * feat(sql): change ddl fit to change
    
    * fix: delete jpa pk generation to use UUID
    
    * feat: change mapping information
    
    * feat: add algorithm code
    
    * chore: formatting
    
    * chore: add .gitignore jpa.lib
    
    * feat: add pre commit hook with spotless
    
    * chore: format with ktfmt
    
    * chore: change format to kotlin lang-style
    
    * chore: change format to kotlin lang-style
    
    * chore: change format to kotlin lang-style
    
    * feat: create method by portone
    
    * feat: create requestPayment
    
    * feat: create checkPayment
    
    * feat: create refundPaymentById
    
    * feat: create refundPayment
    
    * style: format kotlin code style
    
    * fix: add transactional
    
    * fix: optimize file
    
    * feat: add swagger form
    
    * fix: add requestEntity and exception
    
    * style: change kotline lang style
    
    * refactor: change dao package location
    
    ---------
    
    Co-authored-by: SangMinLeeAI <sangmin.lee.ai@gmail.com>

--- src/main/kotlin/uoslife/servermeeting/meetingteam/entity/Payment.kt
+++ src/main/kotlin/uoslife/servermeeting/meetingteam/entity/Payment.kt
@@ -1,32 +1,51 @@
 package uoslife.servermeeting.meetingteam.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.ConstraintMode
-import jakarta.persistence.Entity
-import jakarta.persistence.EnumType
-import jakarta.persistence.Enumerated
-import jakarta.persistence.ForeignKey
-import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.OneToOne
-import jakarta.persistence.Table
+import jakarta.persistence.*
 import java.time.LocalDateTime
 import java.util.UUID
 import uoslife.servermeeting.global.common.BaseEntity
+import uoslife.servermeeting.meetingteam.entity.enums.PayMethod
+import uoslife.servermeeting.meetingteam.entity.enums.PaymentGateway
 import uoslife.servermeeting.meetingteam.entity.enums.PaymentStatus
 import uoslife.servermeeting.user.entity.User
 
 @Entity
 @Table(name = "payment")
 class Payment(
-    @Id @Column(name = "id", nullable = false) var id: UUID? = null,
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    var id: UUID? = null,
     @OneToOne
     @JoinColumn(name = "payment_id", foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT))
     var user: User? = null,
-    var date: LocalDateTime? = null,
-    var amount: Int? = null,
-    var address: String? = null,
-    var name: String? = null,
-    var payedUserId: UUID? = null,
-    @Enumerated(EnumType.STRING) var status: PaymentStatus? = null,
-) : BaseEntity()
+    @Enumerated(EnumType.STRING) var pg: PaymentGateway? = null,
+    @Enumerated(EnumType.STRING) var payMethod: PayMethod? = null,
+    var marchantUid: String? = null,
+    var price: Int? = null,
+    var impUid: String? = null,
+    var paidDate: LocalDateTime? = null,
+    @Enumerated(EnumType.STRING) var status: PaymentStatus = PaymentStatus.NONE,
+) : BaseEntity() {
+    companion object {
+        fun createPayment(
+            user: User,
+            pg: PaymentGateway,
+            payMethod: PayMethod,
+            marchantUid: String,
+            price: Int,
+            status: PaymentStatus,
+        ): Payment {
+            return Payment(
+                user = user,
+                pg = pg,
+                payMethod = payMethod,
+                marchantUid = marchantUid,
+                price = price,
+                impUid = null,
+                paidDate = null,
+                status = status,
+            )
+        }
+    }
+}
---
+++ src/main/kotlin/uoslife/servermeeting/user/dao/UserDao.kt
@@ -0,0 +1,25 @@
+package uoslife.servermeeting.user.dao
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.stereotype.Repository
+import uoslife.servermeeting.meetingteam.entity.MeetingTeam
+import uoslife.servermeeting.meetingteam.entity.QMeetingTeam.meetingTeam
+import uoslife.servermeeting.meetingteam.entity.enums.PaymentStatus
+import uoslife.servermeeting.user.entity.QUser.user
+import uoslife.servermeeting.user.entity.User
+
+@Repository
+class UserDao(
+    private val queryFactory: JPAQueryFactory,
+) {
+    fun findNotMatchedUserInMeetingTeam(meetingTeamList: List<MeetingTeam>): List<User> {
+        return queryFactory
+            .selectFrom(user)
+            .leftJoin(user.team, meetingTeam)
+            .fetchJoin()
+            .where(
+                user.payment.status.eq(PaymentStatus.SUCCESS).and(meetingTeam.`in`(meetingTeamList))
+            )
+            .fetch()
+    }
+}
