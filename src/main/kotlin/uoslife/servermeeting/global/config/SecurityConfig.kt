b0a6d52c41944f5a822e21117265cac02e2166a4
UDADDY <gustmd5715@gmail.com>
2024-03-06 14:11:27 +0900

    chore: apply spotless

--- src/main/kotlin/uoslife/servermeeting/global/config/SecurityConfig.kt
+++ src/main/kotlin/uoslife/servermeeting/global/config/SecurityConfig.kt
@@ -8,10 +8,8 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.CorsUtils
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
