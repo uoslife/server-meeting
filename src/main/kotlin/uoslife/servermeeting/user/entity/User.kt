173272b80207be8708adf49f898ef6173a40dda6
UDADDY <gustmd5715@gmail.com>
2024-03-09 17:35:37 +0900

    feat: 유저 이메일 유니크 속성 추가

--- src/main/kotlin/uoslife/servermeeting/user/entity/User.kt
+++ src/main/kotlin/uoslife/servermeeting/user/entity/User.kt
@@ -16,9 +16,9 @@ class User(
     var phoneNumber: String?,
     @Column(nullable = true, unique = true) var nickname: String? = null,
     var name: String?,
-    var email: String? = null,
+    @Column(nullable = false, unique = true) val email: String,
     var kakaoTalkId: String?,
-    var university: University? = null,
+    var university: University,
     @Type(JsonType::class)
     @Column(columnDefinition = "jsonb")
     var userPersonalInformation: UserPersonalInformation = UserPersonalInformation(),
