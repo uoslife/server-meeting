6ac23ca4c5f5206c14d05bfeb1d14273d83db4b3
UDADDY <gustmd5715@gmail.com>
2024-03-09 21:28:33 +0900

    chore: 미사용 메서드 삭제

--- src/main/kotlin/uoslife/servermeeting/global/auth/service/AuthService.kt
+++ src/main/kotlin/uoslife/servermeeting/global/auth/service/AuthService.kt
@@ -7,10 +7,10 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import uoslife.servermeeting.global.auth.dto.response.TokenResponse
 import uoslife.servermeeting.global.auth.exception.InvalidTokenException
-import uoslife.servermeeting.global.auth.exception.UserNotFoundException
 import uoslife.servermeeting.global.auth.jwt.TokenProvider
 import uoslife.servermeeting.global.auth.jwt.TokenType
 import uoslife.servermeeting.user.entity.User
+import uoslife.servermeeting.user.exception.UserNotFoundException
 import uoslife.servermeeting.user.repository.UserRepository
 import uoslife.servermeeting.user.service.UserService
 import uoslife.servermeeting.verification.service.VerificationService
@@ -37,43 +37,4 @@ class AuthService(
 
         return tokenResponse
     }
-
-    //    @Transactional
-    //    fun logout(userId: Long) {
-    //        logger.info("logout user: $userId")
-    //        val logoutUser = userRepository.findByIdOrNull(userId) ?: throw
-    // UserNotFoundException()
-    //        deviceService.deleteDeviceSecret(logoutUser.device?.id ?: throw
-    // UnauthorizedException())
-    //        smsVerificationRedisRepository.deleteById(logoutUser.phone)
-    //    }
-
-    //    @Transactional
-    //    fun unregisterUser(userId: Long) {
-    //        logger.info("unregister user: $userId")
-    //        val user = userRepository.findByIdOrNull(userId) ?: throw UserNotFoundException()
-    //        deleteUserDeviceIfExists(user)
-    //        deleteUserTosIfExists(user)
-    //        deleteUserPortalAccountIfExists(user)
-    //        userRepository.delete(user)
-    //    }
-
-    //    private fun setUserTos(tosDto: TosDto, user: User?) {
-    //        user?.tos =
-    //            tosRepository.save(
-    //                Tos(
-    //                    privatePolicy = tosDto.privatePolicy ?: false,
-    //                    termsOfUse = tosDto.termsOfUse ?: false,
-    //                    notification = tosDto.notification ?: false,
-    //                    marketing = tosDto.marketing ?: false,
-    //                )
-    //            )
-    //
-    //        tosDto
-    //            .takeIf { it.marketing == true }
-    //            ?.let { subscribeToTopic(user, TopicName.MARKETING_NOTIFICATION.name) }
-    //
-    //        tosDto.takeIf { it.notification == true }?.let {
-    // subscribeToAnnouncementNotification(user) }
-    //    }
 }
