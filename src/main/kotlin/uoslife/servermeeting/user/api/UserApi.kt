e2b46bf540a2f9aaad5d1a11ae9544609417f43c
UDADDY <gustmd5715@gmail.com>
2024-03-09 21:40:13 +0900

    chore: apply spotless

--- src/main/kotlin/uoslife/servermeeting/user/api/UserApi.kt
+++ src/main/kotlin/uoslife/servermeeting/user/api/UserApi.kt
@@ -8,7 +8,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
