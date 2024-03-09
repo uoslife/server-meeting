51efe0807c33c90192b13ecc52680b0e53c6d93b
UDADDY <gustmd5715@gmail.com>
2024-03-09 17:42:08 +0900

    feat: 이메일로 유저 찾기

--- src/main/kotlin/uoslife/servermeeting/user/repository/UserRepository.kt
+++ src/main/kotlin/uoslife/servermeeting/user/repository/UserRepository.kt
@@ -7,4 +7,5 @@ import uoslife.servermeeting.user.entity.User
 interface UserRepository : JpaRepository<User, UUID> {
     fun findUserByNickname(nickname: String): User?
     fun existsByEmail(email: String): Boolean
+    fun findByEmail(email: String): User?
 }
