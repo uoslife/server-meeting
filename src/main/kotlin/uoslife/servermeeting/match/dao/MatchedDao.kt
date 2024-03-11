5f1304c8a97c8c545aa616e183c436c236d0e4d1
InGyu Na <43513001+sktmdgus1212@users.noreply.github.com>
2024-03-11 16:28:19 +0900

    refactor: test meetingTeam and match (#49)
    * feat: hibernate-types for postgres jsonb type
    
    * feat: change entity to jsonb columns
    
    * feat: change service and test followed by entity changes
    
    * feat: change meeting team column
    
    - make information jsonb with dynamic json mapping
    - delete preference and integrate with information columns
    
    * test: change test code due to change in information type
    
    * feat: add email and verified field for school verification
    
    * feat: add payment to userTeam
    
    * feat: add pk to userTeam
    
    * feat: set foreign key in payments to meeting team not in user team
    
    * feat: change compatibility to jsonb
    
    * feat(sql): change ddl fit to change
    
    * fix: delete jpa pk generation to use UUID
    
    * feat: change mapping information
    
    * feat: add algorithm code
    
    * chore: formatting
    
    * chore: add .gitignore jpa.lib
    
    * feat: add pre commit hook with spotless
    
    * chore: format with ktfmt
    
    * chore: change format to kotlin lang-style
    
    * chore: change format to kotlin lang-style
    
    * chore: change format to kotlin lang-style
    
    * fix: update findUserWithMeeting Query
    
    * fix: update param in findUserWithMeetingTeam from single to triple
    
    * fix: add religion in UserFindResponse
    
    * style: kotlin lang style
    
    * fix: delete test file
    
    * fix: fix eror in query
    
    * fix: delete exception in toMeetingTeamInformationGetResponse
    
    * style: kotlin lang style
    
    ---------
    
    Co-authored-by: SangMinLeeAI <sangmin.lee.ai@gmail.com>

--- src/main/kotlin/uoslife/servermeeting/match/dao/MatchedDao.kt
+++ src/main/kotlin/uoslife/servermeeting/match/dao/MatchedDao.kt
@@ -3,5 +3,34 @@ package uoslife.servermeeting.match.dao
 import com.querydsl.jpa.impl.JPAQueryFactory
 import jakarta.transaction.Transactional
 import org.springframework.stereotype.Repository
+import uoslife.servermeeting.match.entity.Match
+import uoslife.servermeeting.match.entity.QMatch.match
+import uoslife.servermeeting.meetingteam.entity.MeetingTeam
+import uoslife.servermeeting.meetingteam.entity.QMeetingTeam.meetingTeam
+import uoslife.servermeeting.user.entity.QUser.user
 
-@Repository @Transactional class MatchedDao(private val queryFactory: JPAQueryFactory) {}
+@Repository
+@Transactional
+class MatchedDao(private val queryFactory: JPAQueryFactory) {
+    fun findMatchByMaleTeamWithFemaleTeam(maleTeam: MeetingTeam): Match? {
+        return queryFactory
+            .selectFrom(match)
+            .join(match.femaleTeam, meetingTeam)
+            .fetchJoin()
+            .join(meetingTeam.leader, user)
+            .fetchJoin()
+            .where(match.maleTeam.eq(maleTeam))
+            .fetchOne()
+    }
+
+    fun findMatchByFeMaleTeamWithMaleTeam(femaleTeam: MeetingTeam): Match? {
+        return queryFactory
+            .selectFrom(match)
+            .join(match.maleTeam, meetingTeam)
+            .fetchJoin()
+            .join(meetingTeam.leader, user)
+            .fetchJoin()
+            .where(match.femaleTeam.eq(femaleTeam))
+            .fetchOne()
+    }
+}
