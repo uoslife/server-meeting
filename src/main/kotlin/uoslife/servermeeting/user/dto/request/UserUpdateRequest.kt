d746375a84e7938419b14bf71a972e6510dbb5eb
naingyu <sktmdgus1212@naver.com>
2024-03-16 23:43:37 +0900

    feat: apply validation in verification, match, user

--- src/main/kotlin/uoslife/servermeeting/user/dto/request/UserUpdateRequest.kt
+++ src/main/kotlin/uoslife/servermeeting/user/dto/request/UserUpdateRequest.kt
@@ -1,18 +1,19 @@
 package uoslife.servermeeting.user.dto.request
 
-import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
 import uoslife.servermeeting.user.entity.enums.*
 
 data class UserUpdateRequest(
-    @NotBlank val name: String,
-    @NotBlank val gender: GenderType,
+    @field:NotNull val name: String,
+    @field:NotNull val gender: GenderType,
     // TODO: PHONENUMBER UI 추가 요청
-    @NotBlank val phoneNumber: String,
-    @NotBlank val age: Int,
-    @NotBlank val height: Int,
-    @NotBlank val kakaoTalkId: String,
-    @NotBlank val department: String,
-    @NotBlank val studentType: StudentType,
+    @field:NotNull @field:Size(max = 11) val phoneNumber: String,
+    @field:NotNull val age: Int,
+    @field:NotNull val kakaoTalkId: String,
+    @field:NotNull val department: String,
+    @field:NotNull val studentType: StudentType,
+    val height: Int?,
     val religion: ReligionType?,
     val smoking: SmokingType?,
     val drinkingMin: Int?,
