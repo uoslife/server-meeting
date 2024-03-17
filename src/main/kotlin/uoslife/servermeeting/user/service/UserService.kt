ddb9d0b9de177b688867ebf83ac89d6ed5878902
UDADDY <gustmd5715@gmail.com>
2024-03-17 20:09:10 +0900

    refactor: 유저 개인정보 업데이트 메서드화

--- src/main/kotlin/uoslife/servermeeting/user/service/UserService.kt
+++ src/main/kotlin/uoslife/servermeeting/user/service/UserService.kt
@@ -35,36 +35,42 @@ class UserService(
         val existingUser =
             userRepository.findByIdOrNull(id) ?: throw ExistingUserNotFoundException()
 
-        val userPersonalInformation =
-            UserPersonalInformation(
-                age = requestDto.age ?: existingUser.userPersonalInformation.age,
-                gender = requestDto.gender ?: existingUser.userPersonalInformation.gender,
-                height = requestDto.height ?: existingUser.userPersonalInformation.height,
-                studentType = requestDto.studentType
-                        ?: existingUser.userPersonalInformation.studentType,
-                kakaoTalkId = requestDto.kakaoTalkId
-                        ?: existingUser.userPersonalInformation.kakaoTalkId,
-                university = existingUser.userPersonalInformation.university,
-                department = requestDto.department
-                        ?: existingUser.userPersonalInformation.department,
-                religion = requestDto.religion ?: existingUser.userPersonalInformation.religion,
-                drinkingMin = requestDto.drinkingMin
-                        ?: existingUser.userPersonalInformation.drinkingMin,
-                drinkingMax = requestDto.drinkingMax
-                        ?: existingUser.userPersonalInformation.drinkingMax,
-                smoking = requestDto.smoking ?: existingUser.userPersonalInformation.smoking,
-                spiritAnimal = requestDto.spiritAnimal
-                        ?: existingUser.userPersonalInformation.spiritAnimal,
-                mbti = requestDto.mbti ?: existingUser.userPersonalInformation.mbti,
-                interest = requestDto.interest ?: existingUser.userPersonalInformation.interest,
-                message = requestDto.message ?: existingUser.userPersonalInformation.message,
-            )
+        val userPersonalInformation: UserPersonalInformation = updateUserPersonalInformationWithDto(existingUser, requestDto)
 
         existingUser.name = requestDto.name ?: existingUser.name
         existingUser.phoneNumber = requestDto.phoneNumber ?: existingUser.phoneNumber
         existingUser.kakaoTalkId = requestDto.kakaoTalkId ?: existingUser.kakaoTalkId
         existingUser.userPersonalInformation = userPersonalInformation
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build()
+    }
+
+    private fun updateUserPersonalInformationWithDto(existingUser: User, requestDto: UserUpdateRequest): UserPersonalInformation{
+        val userPersonalInformation =
+            UserPersonalInformation(
+                age = requestDto.age ?: existingUser.userPersonalInformation.age,
+                gender = requestDto.gender ?: existingUser.userPersonalInformation.gender,
+                height = requestDto.height ?: existingUser.userPersonalInformation.height,
+                studentType = requestDto.studentType
+                    ?: existingUser.userPersonalInformation.studentType,
+                kakaoTalkId = requestDto.kakaoTalkId
+                    ?: existingUser.userPersonalInformation.kakaoTalkId,
+                university = existingUser.userPersonalInformation.university,
+                department = requestDto.department
+                    ?: existingUser.userPersonalInformation.department,
+                religion = requestDto.religion ?: existingUser.userPersonalInformation.religion,
+                drinkingMin = requestDto.drinkingMin
+                    ?: existingUser.userPersonalInformation.drinkingMin,
+                drinkingMax = requestDto.drinkingMax
+                    ?: existingUser.userPersonalInformation.drinkingMax,
+                smoking = requestDto.smoking ?: existingUser.userPersonalInformation.smoking,
+                spiritAnimal = requestDto.spiritAnimal
+                    ?: existingUser.userPersonalInformation.spiritAnimal,
+                mbti = requestDto.mbti ?: existingUser.userPersonalInformation.mbti,
+                interest = requestDto.interest ?: existingUser.userPersonalInformation.interest,
+                message = requestDto.message ?: existingUser.userPersonalInformation.message,
+            )
+
+        return userPersonalInformation
     }
 
     @Transactional
