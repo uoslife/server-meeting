0a667357e6277f5fcf73f58e2fefca7e0dc84125
UDADDY <gustmd5715@gmail.com>
2024-03-18 14:21:20 +0900

    feat: 전화번호로 유저 중복 확인

--- src/main/kotlin/uoslife/servermeeting/user/repository/UserRepository.kt
+++ src/main/kotlin/uoslife/servermeeting/user/repository/UserRepository.kt
@@ -7,4 +7,5 @@ import uoslife.servermeeting.user.entity.User
 interface UserRepository : JpaRepository<User, UUID> {
     fun existsByEmail(email: String): Boolean
     fun findByEmail(email: String): User?
+    fun existsByPhoneNumber(phoneNumber: String): Boolean
 }
