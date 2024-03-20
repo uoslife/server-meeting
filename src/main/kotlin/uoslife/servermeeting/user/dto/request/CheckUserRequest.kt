e8b4353907d63b4bfd880e3273a273c79da7585e
UDADDY <gustmd5715@gmail.com>
2024-03-20 22:28:55 +0900

    chore: apply lint

--- src/main/kotlin/uoslife/servermeeting/user/dto/request/CheckUserRequest.kt
+++ src/main/kotlin/uoslife/servermeeting/user/dto/request/CheckUserRequest.kt
@@ -1,7 +1,6 @@
 package uoslife.servermeeting.user.dto.request
 
 import io.swagger.v3.oas.annotations.media.Schema
-import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotEmpty
 
 data class CheckUserRequest(
