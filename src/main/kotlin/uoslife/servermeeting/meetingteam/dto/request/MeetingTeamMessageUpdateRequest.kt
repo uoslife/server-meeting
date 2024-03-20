46f40a3274e370f577ebf39fd3ae001a5e861c9b
InGyu Na <43513001+sktmdgus1212@users.noreply.github.com>
2024-03-21 08:04:58 +0900

    refactor: add schema in request (#63)
    * feat: hibernate-types for postgres jsonb type
    
    * feat: change entity to jsonb columns
    
    * feat: change service and test followed by entity changes
    
    * feat: change meeting team column
    
    - make information jsonb with dynamic json mapping
    - delete preference and integrate with information columns
    
    * test: change test code due to change in information type
    
    * feat: add email and verified field for school verification
    
    * feat: add payment to userTeam
    
    * feat: add pk to userTeam
    
    * feat: set foreign key in payments to meeting team not in user team
    
    * feat: change compatibility to jsonb
    
    * feat(sql): change ddl fit to change
    
    * fix: delete jpa pk generation to use UUID
    
    * feat: change mapping information
    
    * feat: add algorithm code
    
    * chore: formatting
    
    * chore: add .gitignore jpa.lib
    
    * feat: add pre commit hook with spotless
    
    * chore: format with ktfmt
    
    * chore: change format to kotlin lang-style
    
    * chore: change format to kotlin lang-style
    
    * chore: change format to kotlin lang-style
    
    * refactor: updateUser and createMeetingTeam
    
    * refactor: joinMeetingTeam and getMeetingTeamUserList
    
    * refactor: updateMeetingTeamInformation
    
    * refactor: updateMeetingTeamPreference
    
    * refactor: getMeetingTeamInformation
    
    * feat: getMeetingTeamInformationByKakaoId
    
    * fix: solve conflict
    
    * style: change kotlin lang style
    
    * fix: delete trash file
    
    * fix: delete nickname and change api endpoint
    
    * fix: update code based pr review
    
    * feat: how to update meetingTeamInformationUpdateReqeust?
    
    * refactor: decide MeetingTeamInformationUpdateRequest
    
    * style: kotlin lang style
    
    * docs: write exception in swagger
    
    * style: kotlin lang style
    
    * feat: apply validation in verification, match, user
    
    * fix: teamName can be  null
    
    * refactor: add field schema
    
    * fix: change type
    
    * style: kotlin lang style
    
    * fix: update schema example
    
    * style: kotlin lang style
    
    ---------
    
    Co-authored-by: SangMinLeeAI <sangmin.lee.ai@gmail.com>

--- src/main/kotlin/uoslife/servermeeting/meetingteam/dto/request/MeetingTeamMessageUpdateRequest.kt
+++ src/main/kotlin/uoslife/servermeeting/meetingteam/dto/request/MeetingTeamMessageUpdateRequest.kt
@@ -1,5 +1,10 @@
 package uoslife.servermeeting.meetingteam.dto.request
 
+import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.Size
 
-class MeetingTeamMessageUpdateRequest(@field:Size(min = 10) val message: String?)
+class MeetingTeamMessageUpdateRequest(
+    @Schema(description = "상대에게 보내는 메세지", example = "안녕하세요")
+    @field:Size(min = 10)
+    val message: String?
+)
--- src/main/kotlin/uoslife/servermeeting/meetingteam/dto/request/PaymentRequestDto.kt
+++ src/main/kotlin/uoslife/servermeeting/meetingteam/dto/request/PaymentRequestDto.kt
@@ -1,19 +1,30 @@
 package uoslife.servermeeting.meetingteam.dto.request
 
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Pattern
 import uoslife.servermeeting.meetingteam.entity.enums.PayMethod
 import uoslife.servermeeting.meetingteam.entity.enums.PaymentGateway
 
 class PaymentRequestDto {
     data class PaymentRequestRequest(
-        val pg: PaymentGateway,
-        val payMethod: PayMethod,
+        @Schema(description = "결제 플랫폼", example = "kakaopay") @field:NotNull val pg: PaymentGateway,
+        @Schema(description = "결제 수단", example = "card") @field:NotNull val payMethod: PayMethod,
     )
 
     data class PaymentCheckRequest(
-        val impUid: String,
+        @Schema(description = "결제 고유 번호") @field:NotBlank val impUid: String,
     )
 
     data class PaymentRefundRequest(
+        @field:NotBlank
+        @field:Pattern(
+            regexp =
+                "\\b[A-Za-z0-9._%+-]+@khu\\.ac\\.kr\\b|\\b[A-Za-z0-9._%+-]+@hufs\\.ac\\.kr\\b|\\b[A-Za-z0-9._%+-]+@uos\\.ac\\.kr\\b",
+            message = "Invalid Email"
+        )
+        @Schema(description = "이메일", example = "uoslife123@gmail.com")
         val email: String,
     )
 }
