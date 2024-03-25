57afc636ee65b3a2f2d83a971a1088614ed18fe6
naingyu <sktmdgus1212@naver.com>
2024-03-25 10:29:39 +0900

    fix: delete compatibility proiority

--- src/main/kotlin/uoslife/servermeeting/match/repository/CompatibilityPriorityRepository.kt
+++
@@ -1,6 +0,0 @@
-package uoslife.servermeeting.match.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import uoslife.servermeeting.match.entity.CompatibilityPriority
-
-interface CompatibilityPriorityRepository : JpaRepository<CompatibilityPriority, Long>
