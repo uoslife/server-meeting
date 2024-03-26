a8307e2f2a629c9079e02aa3c341e282c0b85e1e
UDADDY <gustmd5715@gmail.com>
2024-03-26 12:12:32 +0900

    refactor: 미사용 변수 제거

--- src/main/kotlin/uoslife/servermeeting/global/auth/dto/response/UserProfileVO.kt
+++ src/main/kotlin/uoslife/servermeeting/global/auth/dto/response/UserProfileVO.kt
@@ -3,16 +3,6 @@ package uoslife.servermeeting.global.auth.dto.response
 import java.time.LocalDate
 
 data class UserProfileVO(
-    val id: Long?,
-    val name: String, // 이것들만 사용
-    val nickname: String?,
-    val birthday: LocalDate?,
-    val phone: String, // 이것들만 사용
-    val avatarUrl: String?,
-    var isVerified: Boolean? = null,
-    var degree: String? = null,
-    var enrollmentStatus: String? = null,
-    var studentId: String? = null,
-    var departmentName: String? = null,
-    var collegeName: String? = null
+    val name: String,
+    val phone: String,
 )
