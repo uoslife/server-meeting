7e8afed01ea5430f9bf5a0605068d3d865d9e609
UDADDY <gustmd5715@gmail.com>
2024-03-29 16:07:38 +0900

    test: 토큰 검사 미실시 리스트 테스트

--- src/main/kotlin/uoslife/servermeeting/global/config/SecurityConfig.kt
+++ src/main/kotlin/uoslife/servermeeting/global/config/SecurityConfig.kt
@@ -131,7 +131,7 @@ class SecurityConfig(
                     "/swagger-ui.html",
                     "/swagger-ui/**",
                     "/api-docs/**",
-                    "/v3/api-docs/**",
+                    "/v3/api-docs/**/21312321",
                     "/swagger-resources/**",
                     "/test",
                 )
