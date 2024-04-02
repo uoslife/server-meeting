7ad50e7e4db4d868865baf2a4a5a75bd62749fb0
sktmdgus1212 <sktmdgus1212@naver.com>
2024-04-02 10:09:19 +0900

    feat: create healthIndicator

---
+++ src/main/kotlin/uoslife/servermeeting/global/actuator/CustomHealthIndicator.kt
@@ -0,0 +1,20 @@
+package uoslife.servermeeting.global.actuator
+
+import org.springframework.boot.actuate.health.Health
+import org.springframework.boot.actuate.health.HealthIndicator
+import org.springframework.stereotype.Component
+
+@Component
+class CustomHealthIndicator: HealthIndicator {
+    override fun health(): Health {
+        val errorCode = healthCheck()
+        if (errorCode != 0) {
+            return Health.down().withDetail("errorCode", errorCode).build()
+        }
+        return Health.up().build()
+    }
+
+    private fun healthCheck(): Int {
+        return 0
+    }
+}
