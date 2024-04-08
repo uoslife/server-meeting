f77ff8cccba07d822c5e7a97c9b6cc2e3a11ded4
UDADDY <gustmd5715@gmail.com>
2024-04-08 22:42:40 +0900

    refactor: EmailService 추상화

--- src/main/kotlin/uoslife/servermeeting/verification/service/VerificationServiceImpl.kt
+++ src/main/kotlin/uoslife/servermeeting/verification/service/VerificationServiceImpl.kt
@@ -5,14 +5,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import software.amazon.awssdk.services.sesv2.SesV2Client
-import software.amazon.awssdk.services.sesv2.model.Body
-import software.amazon.awssdk.services.sesv2.model.Content
-import software.amazon.awssdk.services.sesv2.model.Destination
-import software.amazon.awssdk.services.sesv2.model.EmailContent
-import software.amazon.awssdk.services.sesv2.model.Message
-import software.amazon.awssdk.services.sesv2.model.SendEmailRequest
-import software.amazon.awssdk.services.sesv2.model.SesV2Exception
+import software.amazon.awssdk.services.sesv2.model.SendEmailResponse
 import uoslife.servermeeting.global.auth.dto.response.TokenResponse
 import uoslife.servermeeting.global.auth.jwt.TokenProvider
 import uoslife.servermeeting.meetingteam.util.UniqueCodeGenerator
@@ -31,15 +24,15 @@ import uoslife.servermeeting.verification.repository.VerificationRedisRepository
 class VerificationServiceImpl(
     private val verificationRedisRepository: VerificationRedisRepository,
     private val userRepository: UserRepository,
-    private val sesV2Client: SesV2Client,
+    private val emailService: EmailService,
     private val uniqueCodeGenerator: UniqueCodeGenerator,
     private val tokenProvider: TokenProvider,
     @Value("\${cloud.aws.ses.from}") private val mailFrom: String
 ): VerificationService {
     companion object {
-        const val SUBJECT: String = "[시대팅] 인증 메일 코드를 확인해주세요"
         private val logger = LoggerFactory.getLogger(VerificationServiceImpl::class.java)
     }
+
     @Transactional
     override fun sendMail(
         verificationCodeSendRequest: VerificationCodeSendRequest
@@ -54,58 +47,10 @@ class VerificationServiceImpl(
         // Cache에 인증코드 저장
         verificationRedisRepository.save(verification)
 
-        // 메일 내용 생성
-        val sendEmailRequest: SendEmailRequest =
-            createSendEmailRequest(verificationCodeSendRequest.email, code)
-
-        // 메일 보내기
-        try {
-            sesV2Client.sendEmail(sendEmailRequest)
-            logger.info(
-                "Verification mail is sended from $mailFrom to ${verificationCodeSendRequest.email}"
-            )
-        } catch (e: SesV2Exception) {
-            logger.error("Email Send Failed Exception")
-            logger.error(e.message)
-            throw EmailSendFailedException()
-        } catch (e: Exception) {
-            logger.error("Email Send Failed Exception")
-            logger.error(e.message)
-            throw EmailSendFailedException()
-        }
+        // 메일 전송
+        val sendEmailResponse: SendEmailResponse = emailService.sendEmail(verificationCodeSendRequest.email, code)
 
         return VerificationCodeSendResponse(true)
-    }
-
-    private fun createSendEmailRequest(to: String, code: String): SendEmailRequest {
-        // 수신 이메일 설정
-        val destination: Destination = Destination.builder().toAddresses(to).build()
-
-        // 메일 내용 설정
-        val emailContent: EmailContent = createEmailContent(code)
-
-        val sendEmailRequest: SendEmailRequest =
-            SendEmailRequest.builder()
-                .fromEmailAddress(mailFrom)
-                .destination(destination)
-                .content(emailContent)
-                .build()
-
-        return sendEmailRequest
-    }
-
-    private fun createContent(content: String): Content {
-        return Content.builder().data(content).charset("UTF-8").build()
-    }
-
-    private fun createEmailContent(code: String): EmailContent {
-        val message: Message =
-            Message.builder()
-                .subject(createContent(SUBJECT))
-                .body(Body.builder().html(createContent(getVerificationMessage(code))).build())
-                .build()
-
-        return EmailContent.builder().simple(message).build()
     }
 
     private fun getOrCreateVerification(email: String): Verification {
@@ -113,10 +58,6 @@ class VerificationServiceImpl(
             verificationRedisRepository.findByIdOrNull(email) ?: Verification.create(email)
 
         return verification
-    }
-
-    private fun getVerificationMessage(code: String): String {
-        return String.format("<h3 style='text-align:cetner;'>인증코드: %s</h3>", code)
     }
 
     @Transactional
