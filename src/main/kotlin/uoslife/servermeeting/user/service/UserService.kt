15c78f2ff1ed1ce4840a9f86071c32e5be8954a5
sktmdgus1212 <sktmdgus1212@naver.com>
2024-05-13 18:20:15 +0900

    style: kotlin lang style

--- src/main/kotlin/uoslife/servermeeting/user/service/UserService.kt
+++ src/main/kotlin/uoslife/servermeeting/user/service/UserService.kt
@@ -1,8 +1,6 @@
 package uoslife.servermeeting.user.service
 
 import java.util.*
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.*
 import org.springframework.stereotype.Service
