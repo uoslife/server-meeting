a4020c6ade564309e7525368dd312afb761c0cd6
UDADDY <gustmd5715@gmail.com>
2024-05-18 23:25:19 +0900

    fix: feign client 분리

--- src/main/kotlin/uoslife/servermeeting/global/auth/service/AccountService.kt
+++ src/main/kotlin/uoslife/servermeeting/global/auth/service/AccountService.kt
@@ -15,35 +15,42 @@ import uoslife.servermeeting.global.auth.dto.response.AccountResponse
 @Service
 class AccountService {
     @Autowired lateinit var serverClient: ServerClient
+    @Autowired lateinit var userClient: UserClient
 
     fun getUserProfile(userId: Long): AccountResponse {
         return serverClient.getUserProfile(userId)
     }
 
     fun getMyProfile(authorizationHeader: String): AccountResponse{
-        return serverClient.getMyProfile(authorizationHeader)
+        return userClient.getMyProfile(authorizationHeader)
     }
 }
 
 @FeignClient(
     name = "uoslife-account-api-server",
     url = "\${account.url}",
-//    configuration = [ServerClient.BasicAuthConfiguration::class]
+    configuration = [ServerClient.BasicAuthConfiguration::class]
 )
 interface ServerClient {
     @GetMapping("/v1/users/{userId}")
     fun getUserProfile(@PathVariable("userId") userId: Long): AccountResponse
 
+    class BasicAuthConfiguration(
+        @Value("\${account.access.id}") private val accessKeyId: String,
+        @Value("\${account.access.secret}") private val accessKeySecret: String,
+    ) {
+        @Bean
+        fun basicAuthRequestInterceptor(): BasicAuthRequestInterceptor {
+            return BasicAuthRequestInterceptor(accessKeyId, accessKeySecret)
+        }
+    }
+}
+
+@FeignClient(
+    name = "uoslife-account-api-user",
+    url = "\${account.url}",
+)
+interface UserClient {
     @GetMapping("/v1/users/me")
     fun getMyProfile(@RequestHeader("Authorization") authorization: String): AccountResponse
-
-//    class BasicAuthConfiguration(
-//        @Value("\${account.access.id}") private val accessKeyId: String,
-//        @Value("\${account.access.secret}") private val accessKeySecret: String,
-//    ) {
-//        @Bean
-//        fun basicAuthRequestInterceptor(): BasicAuthRequestInterceptor {
-//            return BasicAuthRequestInterceptor(accessKeyId, accessKeySecret)
-//        }
-//    }
 }
