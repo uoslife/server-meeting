4fe065c7ef4a203cc4fa6befc3a1e423e25799e0
sktmdgus1212 <sktmdgus1212@naver.com>
2024-05-19 23:11:08 +0900

    fix: id bigint 명시

--- src/main/resources/db/migration/V16__user_id_set_bigint.sql
+++ src/main/resources/db/migration/V16__user_id_set_bigint.sql
@@ -1,8 +1,8 @@
 ALTER TABLE "user"
-ALTER COLUMN id TYPE BIGINT;
+ALTER COLUMN id TYPE BIGINT USING id::bigint;
 
 ALTER TABLE "meeting_team"
-ALTER COLUMN leader_id TYPE BIGINT;
+ALTER COLUMN leader_id TYPE BIGINT USING leader_id::bigint;
 
 ALTER TABLE "payment"
-ALTER COLUMN user_id TYPE BIGINT;
+ALTER COLUMN user_id TYPE BIGINT USING user_id::bigint;
