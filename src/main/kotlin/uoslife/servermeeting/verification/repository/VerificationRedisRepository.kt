2de97d7f6f6b8d7ef34dca1fe41892feef1ca169
UDADDY <gustmd5715@gmail.com>
2024-05-20 02:56:20 +0900

    chore: verification 삭제

--- src/main/kotlin/uoslife/servermeeting/verification/repository/VerificationRedisRepository.kt
+++
@@ -1,7 +0,0 @@
-package uoslife.servermeeting.verification.repository
-
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-import uoslife.servermeeting.verification.entity.Verification
-
-@Repository interface VerificationRedisRepository : CrudRepository<Verification, String> {}
