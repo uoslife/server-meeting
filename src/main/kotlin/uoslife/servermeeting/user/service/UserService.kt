b8a988a6ab8988e8f51bcf7d6e00d41587d244c3
UDADDY <gustmd5715@gmail.com>
2024-05-20 15:35:12 +0900

    chore: apply lint

--- src/main/kotlin/uoslife/servermeeting/user/service/UserService.kt
+++ src/main/kotlin/uoslife/servermeeting/user/service/UserService.kt
@@ -12,7 +12,7 @@ import uoslife.servermeeting.user.dto.response.UserFindResponse
 import uoslife.servermeeting.user.entity.University
 import uoslife.servermeeting.user.entity.User
 import uoslife.servermeeting.user.entity.UserPersonalInformation
-import uoslife.servermeeting.user.exception.EmailUnauthorizedException
+import uoslife.servermeeting.user.exception.UserNotAuthorizedException
 import uoslife.servermeeting.user.exception.UserNotFoundException
 import uoslife.servermeeting.user.repository.UserRepository
 
@@ -31,8 +31,7 @@ class UserService(
         val userProfile = uoslifeAccountService.getUserProfile(id)
 
         // 인증(포털 인증, 이메일 인증) 안 된 경우
-        if (!userProfile.isVerified)
-            throw UserNotAuthorizedException()
+        if (!userProfile.isVerified) throw UserNotAuthorizedException()
 
         // 해당 유저가 처음 이용하는 유저면 유저 생성
         // 그렇지 않으면 유저 조회
