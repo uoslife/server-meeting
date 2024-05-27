20db0c66dd2b37e7c672084f39956eea491a46dd
유현승 <gustmd5715@gmail.com>
2024-05-27 18:10:13 +0900

    fix: 매칭 리더만 볼 수 있도록 변경 (#166)
    * fix: ddl-auto validate로 다시 변경
    
    * feat: V1__init 생성
    
    * fix: 유저 검증 로직 삭제
    
    * fix: sql 로그에서 제거
    
    * feat: 요청 로그
    
    * fix: 결제 취소 시 DB에서 payment 삭제
    
    * feat: 리더만 볼 수 있게 예외 추가
    
    * fix: 리더 아니면 예외 발생
    
    * fix: 응답값 변경
    
    * chore: apply lint
    
    * fix: 변수명 변경

--- src/main/kotlin/uoslife/servermeeting/match/dto/response/MatchedMeetingTeamInformationGetResponse.kt
+++ src/main/kotlin/uoslife/servermeeting/match/dto/response/MatchedMeetingTeamInformationGetResponse.kt
@@ -10,7 +10,7 @@ data class MatchedMeetingTeamInformationGetResponse(
     @field:NotNull @Schema(description = "팀 타입", example = "SINGLE") val teamType: TeamType,
     @Schema(description = "팀 이름", example = "팀 이름(1:1인 경우 null)") val teamName: String?,
     @field:NotNull @Schema(description = "성별", example = "MALE") val gender: GenderType,
-    @Schema(description = "팀에 속한 유저 정보") val teamUserList: List<UserProfile>?,
+    @Schema(description = "팀에 속한 유저 정보") val leaderProfile: UserProfile?,
     @Schema(description = "질문 응답값") val information: MatchedInformation?,
     @Schema(description = "상대에게 전하는 메세지") val message: String?
 )
