1e996dbdd27a6b2274929acbbcf3e7d90218ddf3
23tae <taehoonkim.dev@gmail.com>
2024-11-02 23:28:17 +0900

    feat: Redis 설정

---
+++ src/main/kotlin/uoslife/servermeeting/auth/config/RedisConfig.kt
@@ -0,0 +1,36 @@
+package uoslife.servermeeting.auth.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+class RedisConfig {
+
+    @Value("\${spring.data.redis.host}")
+    private lateinit var host: String
+
+    @Value("\${spring.data.redis.port}")
+    private var port: Int = 0
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        return LettuceConnectionFactory(host, port)
+    }
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<String, Any> {
+        return RedisTemplate<String, Any>().apply {
+            setConnectionFactory(redisConnectionFactory())
+
+            keySerializer = StringRedisSerializer()
+            valueSerializer = StringRedisSerializer()
+            hashKeySerializer = StringRedisSerializer()
+            hashValueSerializer = StringRedisSerializer()
+        }
+    }
+}
