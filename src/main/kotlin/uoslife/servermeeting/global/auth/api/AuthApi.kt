84e4a968a95c17be46a62dd425c09b302cf1e775
23tae <taehoonkim.dev@gmail.com>
2024-11-04 23:36:36 +0900

    feat: 이메일 전송 API

--- src/main/kotlin/uoslife/servermeeting/global/auth/api/AuthApi.kt
+++ src/main/kotlin/uoslife/servermeeting/global/auth/api/AuthApi.kt
@@ -1,9 +1,21 @@
 package uoslife.servermeeting.global.auth.api
 
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import uoslife.servermeeting.global.auth.dto.response.SendVerificationEmailResponse
+import uoslife.servermeeting.global.auth.service.EmailVerificationService
 
 @RestController
 @RequestMapping("/api/auth")
-class AuthApi {
+class AuthApi(
+    private val emailVerificationService: EmailVerificationService
+) {
+    @PostMapping("/send-verification-email")
+    fun sendVerificationEmail(@RequestParam email: String): ResponseEntity<SendVerificationEmailResponse> {
+        val response = emailVerificationService.sendVerificationEmail(email)
+        return ResponseEntity.ok(response)
+    }
 }
