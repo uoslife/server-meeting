9441b0e44de470ae6b2a4c37d306bab72f0e6145
woojin <zaza0804@uos.ac.kr>
2024-11-04 19:11:10 +0900

    feat: add DAO function
    - Payment Dao 함수 추가

--- src/main/kotlin/uoslife/servermeeting/user/dao/UserDao.kt
+++ src/main/kotlin/uoslife/servermeeting/user/dao/UserDao.kt
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository
 import uoslife.servermeeting.meetingteam.entity.Payment
 import uoslife.servermeeting.meetingteam.entity.QPayment.payment
 import uoslife.servermeeting.meetingteam.entity.QSingleMeetingTeam.singleMeetingTeam
+import uoslife.servermeeting.meetingteam.entity.QTripleMeetingTeam.tripleMeetingTeam
 import uoslife.servermeeting.meetingteam.entity.enums.PaymentStatus
 import uoslife.servermeeting.user.entity.QUser.user
 import uoslife.servermeeting.user.entity.User
@@ -17,6 +18,7 @@ class UserDao(
         return queryFactory
             .selectFrom(user)
             .leftJoin(user.singleTeam, singleMeetingTeam)
+            .leftJoin(user.tripleTeam, tripleMeetingTeam)
             .fetchJoin()
             .where(user.id.eq(userId))
             .fetchOne()
@@ -24,7 +26,7 @@ class UserDao(
     fun findNotMatchedPayment(userIdList: List<Long>): List<Payment> {
         return queryFactory
             .selectFrom(payment)
-            .innerJoin(payment.user, user)
+            //            .innerJoin(payment.user, user)
             .fetchJoin()
             .where(payment.status.eq(PaymentStatus.SUCCESS).and(user.id.notIn(userIdList)))
             .fetch()
