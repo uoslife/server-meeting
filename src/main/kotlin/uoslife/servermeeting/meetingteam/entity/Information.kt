cfb0441b0dd5f30ba2d34a2d785edf1a69dde453
woojin <zaza0804@uos.ac.kr>
2024-11-05 19:00:05 +0900

    refactor: change jsonb column to OneToOne Column
    - Meeting Entity에서 Jsonb 컬럼을 전환했습니다

--- src/main/kotlin/uoslife/servermeeting/meetingteam/entity/Information.kt
+++ src/main/kotlin/uoslife/servermeeting/meetingteam/entity/Information.kt
@@ -1,14 +1,24 @@
 package uoslife.servermeeting.meetingteam.entity
 
+import jakarta.persistence.*
 import java.io.Serializable
 import uoslife.servermeeting.match.dto.response.MatchedInformation
 import uoslife.servermeeting.user.entity.enums.GenderType
 
-data class Information(
+@Entity
+class Information(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Long?= null,
+
+    @OneToOne
+    @JoinColumn(name="meeting_team_id")
+    var meetingTeam: MeetingTeam?=null,
+
+    @Enumerated(value = EnumType.STRING)
     val gender: GenderType? = null,
-    val questions: Map<Int, Int>? = null,
-) : Serializable {
+) {
     fun toMatchedInformation(): MatchedInformation {
-        return MatchedInformation(gender = gender, questions = questions?.values?.toList())
+        return MatchedInformation(gender = gender)
     }
 }
