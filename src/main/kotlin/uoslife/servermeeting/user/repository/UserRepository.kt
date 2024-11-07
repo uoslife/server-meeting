228f85e3b25131eb3ddf03bf74af2b5263c728cc
23tae <taehoonkim.dev@gmail.com>
2024-11-07 23:19:53 +0900

    feat: email로 유저 조회

--- src/main/kotlin/uoslife/servermeeting/user/repository/UserRepository.kt
+++ src/main/kotlin/uoslife/servermeeting/user/repository/UserRepository.kt
@@ -2,9 +2,12 @@ package uoslife.servermeeting.user.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import uoslife.servermeeting.user.entity.User
+import java.util.*
 
 interface UserRepository : JpaRepository<User, Long> {
     fun findByPhoneNumber(phoneNumber: String): User?
 
     fun existsByKakaoTalkId(kakaoTalkId: String): Boolean
+
+    fun findByEmail(email: String): Optional<User>
 }
