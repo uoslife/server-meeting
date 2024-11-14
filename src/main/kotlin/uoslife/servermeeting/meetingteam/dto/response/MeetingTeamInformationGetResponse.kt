013d110d1585890794ad0b659083c8f2f9cb38c4
woojin <zaza0804@uos.ac.kr>
2024-11-15 02:10:29 +0900

    feat: 미팅 선호 항목 업데이트
    - Entity 컬럼 수정
    - DTO 항목 수정
    - 순환 참조 방지 DTO 설정

--- src/main/kotlin/uoslife/servermeeting/meetingteam/dto/response/MeetingTeamInformationGetResponse.kt
+++ src/main/kotlin/uoslife/servermeeting/meetingteam/dto/response/MeetingTeamInformationGetResponse.kt
@@ -1,12 +1,15 @@
 package uoslife.servermeeting.meetingteam.dto.response
 
-import com.fasterxml.jackson.annotation.JsonIgnore
 import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.persistence.*
 import jakarta.validation.constraints.NotNull
+import uoslife.servermeeting.match.dto.response.MatchedInformation
 import uoslife.servermeeting.match.dto.response.MatchedMeetingTeamInformationGetResponse
 import uoslife.servermeeting.meetingteam.entity.Information
 import uoslife.servermeeting.meetingteam.entity.Preference
+import uoslife.servermeeting.meetingteam.entity.enums.TeamMood
 import uoslife.servermeeting.meetingteam.entity.enums.TeamType
+import uoslife.servermeeting.meetingteam.entity.enums.Weight
 import uoslife.servermeeting.user.entity.enums.*
 
 data class MeetingTeamInformationGetResponse(
@@ -14,8 +17,8 @@ data class MeetingTeamInformationGetResponse(
     @Schema(description = "팀 이름", example = "팀 이름(1:1인 경우 null)") val teamName: String?,
     @field:NotNull @Schema(description = "성별", example = "MALE") val gender: GenderType,
     @Schema(description = "팀에 속한 유저 정보") val opponentLeaderProfile: UserProfile?,
-    @Schema(description = "질문 응답값") @JsonIgnore val information: Information?,
-    @Schema(description = "상대방 선호 응답값") val preference: Preference?,
+    @Schema(description = "질문 응답값") val information: InformationDto? = null,
+    @Schema(description = "상대방 선호 응답값") val preference: PreferenceDto?,
     @Schema(description = "상대에게 전하는 메세지") val message: String?
 ) {
     fun toMatchedMeetingTeamInformationGetResponse(): MatchedMeetingTeamInformationGetResponse {
@@ -24,9 +27,51 @@ data class MeetingTeamInformationGetResponse(
             teamName = teamName,
             gender = gender,
             leaderProfile = opponentLeaderProfile,
-            information = information?.toMatchedInformation(),
+            information = MatchedInformation(gender = information?.gender),
             message = message
         )
+    }
+}
+
+data class PreferenceDto(
+    var ageMin: Int? = null,
+    var ageMax: Int? = null,
+    var heightMin: Int? = null,
+    var heightMax: Int? = null,
+    var smoking: List<SmokingType>? = null, // todo: List는 엔티티에 어떻게 저장할지
+    var appearanceType: List<AppearanceType>? = null,
+    var eyelidType: List<EyelidType>? = null,
+    var mbti: String? = null,
+    var mood: TeamMood? = null,
+    var weight: Weight? = null,
+    @Schema(description = "피하고 싶은 학번", example = "19") val avoidanceNumber: Int?,
+    @Schema(description = "피하고 싶은 학과", example = "통계학과") val avoidanceDepartment: String?,
+) {
+    companion object {
+        fun valueOf(preference: Preference): PreferenceDto {
+            return PreferenceDto(
+                ageMin = preference.ageMin,
+                ageMax = preference.ageMax,
+                heightMax = preference.heightMax,
+                heightMin = preference.heightMin,
+                smoking = preference.smoking,
+                appearanceType = preference.appearanceType,
+                eyelidType = preference.eyelidType,
+                mbti = preference.mbti,
+                mood = preference.mood,
+                weight = preference.weight,
+                avoidanceNumber = preference.avoidanceNumber,
+                avoidanceDepartment = preference.avoidanceDepartment
+            )
+        }
+    }
+}
+
+data class InformationDto(val gender: GenderType?) {
+    companion object {
+        fun valueOf(information: Information): InformationDto {
+            return InformationDto(gender = information.gender)
+        }
     }
 }
 
