9f167df456b2d5a2fe48965f8619c24b90c934bd
seogwoojin <121532823+seogwoojin@users.noreply.github.com>
2024-11-14 23:30:13 +0900

    feat: 매칭 기능 롤백  (#180)
    * feat: rollback Match Api
    
    - 매칭 결과에 팀 타입 추가
    - Dao 개선
    
    * feat: change exception class naming
    
    - 성별 예외 클래스명 변경
    
    * style: spotless apply 적용

--- src/main/kotlin/uoslife/servermeeting/meetingteam/exception/GenderNotUpdatedException.kt
+++ src/main/kotlin/uoslife/servermeeting/meetingteam/exception/GenderNotUpdatedException.kt
@@ -3,4 +3,4 @@ package uoslife.servermeeting.meetingteam.exception
 import uoslife.servermeeting.global.error.exception.EntityNotFoundException
 import uoslife.servermeeting.global.error.exception.ErrorCode
 
-class GenderNotFoundException : EntityNotFoundException(ErrorCode.GENDER_NOT_FOUND)
+class GenderNotUpdatedException : EntityNotFoundException(ErrorCode.GENDER_NOT_FOUND)
