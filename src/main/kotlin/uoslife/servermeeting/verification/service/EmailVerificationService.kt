7daca2e5c8c7c1a5072fc600e145026f12f76f57
23tae <taehoonkim.dev@gmail.com>
2024-11-16 01:20:37 +0900

    style: 인증메일 디자인 개선

--- src/main/kotlin/uoslife/servermeeting/verification/service/EmailVerificationService.kt
+++ src/main/kotlin/uoslife/servermeeting/verification/service/EmailVerificationService.kt
@@ -106,16 +106,17 @@ class EmailVerificationService(
     }
 
     private fun sendEmail(email: String, verificationCode: String) {
+        val codeExpiryMinutes = codeExpiry / 60
+        val emailBody = String.format(emailTemplate, codeExpiryMinutes, verificationCode)
         val request =
             SendEmailRequest()
                 .withDestination(Destination().withToAddresses(email))
                 .withMessage(
                     Message()
                         .withSubject(Content(emailTitle))
-                        .withBody(Body().withText(Content("인증 코드: $verificationCode")))
+                        .withBody(Body().withHtml(Content(emailBody)))
                 )
                 .withSource(emailFrom)
-
         try {
             sesClient.sendEmail(request)
         } catch (e: Exception) {
@@ -165,4 +166,32 @@ class EmailVerificationService(
         }
         return "$prefix:$email:${LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE)}"
     }
+
+    private val emailTemplate =
+        """
+        <html>
+            <body style="font-family: Arial, sans-serif;">
+                <div style="max-width: 600px; margin: 0 auto; padding: 20px;">
+                    <div style="display: flex; align-items: center; gap: 10px; margin-bottom: 30px;">
+                        <img alt="Logo" src="https://www.uoslife.team/favicon.svg" width="146" height="39" />
+                        <span style="font-size: 19px; font-weight: bold;">시대생</span>
+                    </div>
+
+                    <h1 style="font-size: 34px; margin-bottom: 20px;">인증번호 안내</h1>
+                    <p style="color: #575757;">인증번호의 유효시간은 <strong>%d분</strong>입니다.</p>
+
+                    <div style="border: 2px solid #d1d1d1; padding: 35px; text-align: center; margin: 30px 0;">
+                        <strong style="font-size: 40px; color: #f8605f;">%s</strong>
+                    </div>
+
+                    <footer style="background: #f1f1f1; padding: 25px; margin-top: 30px;">
+                        <a href="https://www.instagram.com/uoslife_official/" style="color: #575757; text-decoration: none; margin-right: 20px; font-size: 12px;">Instagram</a>
+                        <a href="https://www.uoslife.team/" style="color: #575757; text-decoration: none; font-size: 12px;">Blog</a>
+                        <p style="margin-top: 10px; font-size: 12px; color: #575757;">© 2024 시대생팀</p>
+                    </footer>
+                </div>
+            </body>
+        </html>
+    """.trimIndent(
+        )
 }
