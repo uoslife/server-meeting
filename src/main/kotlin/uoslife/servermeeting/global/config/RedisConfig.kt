6e8b97c250b0191fe2f11e9246e15a5784f7e3ca
seogwoojin <121532823+seogwoojin@users.noreply.github.com>
2024-11-25 16:02:58 +0900

    fix: 레디스 패스워드 변경 (#207)
    * feat: 3:3 DTO 유저 응답 값 추가
    
    * refactor: Join API 에서 isJoin 파라미터 제거
    
    * refactor: 결제 로깅 통합
    
    - 결제 상태 변경
    - 결제 서비스 함수 세분화
    
    * build: Docker-compose Redis 분리
    
    * build: add Redis Password
    
    * build: add Redis Password

--- src/main/kotlin/uoslife/servermeeting/global/config/RedisConfig.kt
+++ src/main/kotlin/uoslife/servermeeting/global/config/RedisConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.StringRedisSerializer
@@ -15,11 +16,21 @@ class RedisConfig {
 
     @Value("\${spring.data.redis.port}") private var port: Int = 0
 
+    @Value("\${spring.data.redis.password:}") // 비밀번호가 없는 경우 기본값은 빈 문자열
+    private lateinit var password: String
+
     @Bean
     fun redisConnectionFactory(): RedisConnectionFactory {
-        return LettuceConnectionFactory(host, port)
+        val config =
+            RedisStandaloneConfiguration().apply {
+                this.hostName = host
+                this.port = port
+                if (password.isPresent) { // 비밀번호가 설정된 경우에만 추가
+                    this.password = password
+                }
+            }
+        return LettuceConnectionFactory(config)
     }
-
     @Bean
     fun redisTemplate(): RedisTemplate<String, Any> {
         return RedisTemplate<String, Any>().apply {
