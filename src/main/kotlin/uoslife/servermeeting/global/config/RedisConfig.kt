693ed88a7dd68fa2033e41ebc33e4b0812c8c1c4
woojin <zaza0804@uos.ac.kr>
2024-11-25 16:23:00 +0900

    fix: fix Redis Password input

--- src/main/kotlin/uoslife/servermeeting/global/config/RedisConfig.kt
+++ src/main/kotlin/uoslife/servermeeting/global/config/RedisConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisPassword
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
@@ -12,21 +13,19 @@ import org.springframework.data.redis.serializer.StringRedisSerializer
 @Configuration
 class RedisConfig {
 
-    @Value("\${spring.data.redis.host}") private lateinit var host: String
+    @Value("\${spring.data.redis.host}") private lateinit var redisHost: String
 
-    @Value("\${spring.data.redis.port}") private var port: Int = 0
+    @Value("\${spring.data.redis.port}") private var redisPort: Int = 0
 
     @Value("\${spring.data.redis.password:}") // 비밀번호가 없는 경우 기본값은 빈 문자열
-    private lateinit var password: String
+    private lateinit var redisPassword: String
 
     @Bean
     fun redisConnectionFactory(): RedisConnectionFactory {
         val config =
-            RedisStandaloneConfiguration().apply {
-                this.hostName = host
-                this.port = port
-                if (password.isPresent) { // 비밀번호가 설정된 경우에만 추가
-                    this.password = password
+            RedisStandaloneConfiguration(redisHost, redisPort).apply {
+                if (redisPassword.isNotBlank()) { // 비밀번호가 설정된 경우만 추가
+                    this.password = RedisPassword.of(redisPassword)
                 }
             }
         return LettuceConnectionFactory(config)
