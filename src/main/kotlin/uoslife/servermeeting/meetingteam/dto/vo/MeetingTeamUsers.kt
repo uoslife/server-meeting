a933f6c20520d5a1fdac63cbee0cc059b280bc41
woojin <zaza0804@uos.ac.kr>
2024-11-25 14:46:36 +0900

    feat: 3:3 DTO 유저 응답 값 추가

--- src/main/kotlin/uoslife/servermeeting/meetingteam/dto/vo/MeetingTeamUsers.kt
+++ src/main/kotlin/uoslife/servermeeting/meetingteam/dto/vo/MeetingTeamUsers.kt
@@ -2,6 +2,7 @@ package uoslife.servermeeting.meetingteam.dto.vo
 
 import uoslife.servermeeting.meetingteam.dto.response.MeetingTeamUser
 import uoslife.servermeeting.meetingteam.dto.response.MeetingTeamUserListGetResponse
+import uoslife.servermeeting.meetingteam.exception.UserInfoNotCompletedException
 import uoslife.servermeeting.user.entity.User
 
 class MeetingTeamUsers(private val users: List<User>) {
@@ -10,8 +11,13 @@ class MeetingTeamUsers(private val users: List<User>) {
         return MeetingTeamUserListGetResponse(
             teamName = teamName,
             users.map {
+                it.userInformation ?: throw UserInfoNotCompletedException()
                 MeetingTeamUser(
                     name = it.name,
+                    department = it.userInformation!!.department,
+                    interest = it.userInformation!!.interest,
+                    kakaoTalkId = it.kakaoTalkId,
+                    studentNumber = it.userInformation!!.studentNumber
                 )
             },
         )
