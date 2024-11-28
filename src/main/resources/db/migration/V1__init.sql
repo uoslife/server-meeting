3a1c5aae22b46172cc1271ac1145b1dc4dce42b6
woojin <zaza0804@uos.ac.kr>
2024-11-28 12:47:43 +0900

    build: change ddl-auto
    - 1:1 관계에 제약조건 추가

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,57 +1,8 @@
--- -- Drop tables if they exist
--- DROP TABLE IF EXISTS match;
--- DROP TABLE IF EXISTS meeting_team;
--- DROP TABLE IF EXISTS payment;
--- DROP TABLE IF EXISTS "user";
---
--- -- Create meeting_user table
--- CREATE TABLE meeting_user (
---                               id BIGINT NOT NULL PRIMARY KEY,
---                               created_at TIMESTAMP(6) NOT NULL,
---                               updated_at TIMESTAMP(6) NOT NULL,
---                               kakao_talk_id VARCHAR(255) UNIQUE,
---                               name VARCHAR(255),
---                               phone_number VARCHAR(255),
---                               user_personal_information JSONB,
---                               team_id BIGINT
--- );
---
--- -- Create match table
--- CREATE TABLE match (
---                        id BIGSERIAL PRIMARY KEY,
---                        created_at TIMESTAMP(6) NOT NULL,
---                        updated_at TIMESTAMP(6) NOT NULL,
---                        date TIMESTAMP(6),
---                        female_team_id BIGINT,
---                        male_team_id BIGINT
--- );
---
--- -- Create meeting_team table
--- CREATE TABLE meeting_team (
---                               id BIGSERIAL PRIMARY KEY,
---                               code VARCHAR(255) NOT NULL,
---                               compatibility JSONB,
---                               information JSONB,
---                               message VARCHAR(255),
---                               name VARCHAR(255),
---                               preference JSONB,
---                               season INTEGER,
---                               type VARCHAR(255) NOT NULL,
---                               leader_id BIGINT UNIQUE
--- );
---
--- -- Create payment table
--- CREATE TABLE payment (
---                          id UUID PRIMARY KEY,
---                          created_at TIMESTAMP(6) NOT NULL,
---                          updated_at TIMESTAMP(6) NOT NULL,
---                          imp_uid VARCHAR(255),
---                          marchant_uid VARCHAR(255),
---                          paid_date TIMESTAMP(6),
---                          pay_method VARCHAR(255),
---                          pg VARCHAR(255),
---                          price INTEGER,
---                          status VARCHAR(255),
---                          user_id BIGINT,
---                          FOREIGN KEY (user_id) REFERENCES meeting_user (id)
--- );
+ALTER TABLE preference
+    ADD CONSTRAINT unique_meeting_team UNIQUE (meeting_team_id);
+
+ALTER TABLE meeting_team
+    RENAME COLUMN message TO course;
+
+ALTER TABLE user_information
+    ADD CONSTRAINT unique_user UNIQUE (user_id);
